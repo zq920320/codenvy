f6dffbe88b94785e8dc19d0a796988a6efa70a74
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-01-23 22:37:33 -0800

    update focus tracking for the signup form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,16 +3,24 @@ define(["jquery"], function($){
 	return {
 		run : function(){
 			$(document).ready(function(){
+                function adjustFormDisplay(){
+                    if($(".signup-form > input, a").toArray().indexOf(document.activeElement) !== -1){
+                        $(".signup-form > input[type=submit]").show();
+                        $(".signup-form > .domain-name").removeClass("hidden-text-box");
+                        $(".aternative-login").addClass("collapsed");
+                    } else {
+                        $(".signup-form > input[type=submit]").hide();
+                        $(".signup-form > .domain-name").addClass("hidden-text-box");
+                        $(".aternative-login").removeClass("collapsed");
+                    }
+                }
+
                 $(".signup-form > input, a").on("focus", function(){
-                    $(".signup-form > input[type=submit]").show();
-                    $(".signup-form > .domain-name").removeClass("hidden-text-box");
-                    $(".aternative-login").addClass("collapsed");
+                    adjustFormDisplay();
                 });
 
                 $(".signup-form > input, a").on("blur", function(){
-                    $(".signup-form > input[type=submit]").hide();
-                    $(".signup-form > .domain-name").addClass("hidden-text-box");
-                    $(".aternative-login").removeClass("collapsed");
+                    adjustFormDisplay();
                 });
 			});
 		}
