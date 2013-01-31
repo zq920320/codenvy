820d710c6519b80ac93abb57b76e6a663816b068
Mark Downey <mark.downey@exoplatform.com>
2013-01-31 11:01:09 -0800

    updated jobs,tos and account handler. added legacy pages

--- app/scripts/models/account.js
+++ app/scripts/models/account.js
@@ -55,8 +55,36 @@ define(["jquery"],function($){
 
         },
 
-        createTenant : function(success,error){
+        createTenant : function(email,domain,success,error){
 
+            var tenantServiceConfirmUrl = "/cloud-admin/rest/cloud-admin/public-tenant-service/create-with-confirm";
+            var createTenantUrl = tenantServiceConfirmUrl + "/" + encodeURIComponent(domain) + "/" + email;
+
+            var request = $.ajax(
+                url : createTenantUrl,
+                type: POST,
+            ),
+            success : function(output, status, xhr){
+
+                    //Succes, redirect to thankyou.jsp
+                    success({url: 'thankyou.jsp'});
+            },
+            error : function(xhr, status, err){
+
+                //if redirect status code, redirect to ResponseHeader
+                if(xhr.getReponseHeader("Location") != null) && (xhr.status != "") && (xhr.status == 301 || xhr.status == 302 || xhr.status == 303){
+                    
+                    //redirect to Location
+                    errors.push({url: xhr.getReponseHeader("Location")});
+                }
+
+                //else error code, display error message
+                else {
+                    errors.push(new AccountError("domain","Something went wrong: " + err));
+                    
+                }
+                error(errors);      
+            };
         },
 
         recoverPassword : function(email,success,error){
