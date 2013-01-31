83c02246e02bf9e93185f4f0312fe474cfd3b428
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-01-31 00:17:04 -0800

    refactor app to use sign up form view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,13 @@
-define(["jquery"], function($){
+define(["jquery","views/signupform"], function($,SignupForm){
 
 	return {
 		run : function(){
 			$(document).ready(function(){
-                function adjustFormDisplay(){
-                    if($(".field > input").toArray().indexOf(document.activeElement) !== -1){
-                        $(".field > .signup-button").removeClass("hidden-button");
-                        $(".field > .domain-name").removeClass("hidden-text-box");
-                        $(".aternative-login").addClass("collapsed");
-                    } else {
-                        $(".field > .signup-button").addClass("hidden-button");
-                        $(".field > .domain-name").addClass("hidden-text-box");
-                        $(".aternative-login").removeClass("collapsed");
-                    }
+                var signupForm = $(".signup-form");
+
+                if(signupForm.length !== 0){
+                    SignupForm.get(signupForm);
                 }
-
-                $(".field > input").on("focus", function(){
-                    adjustFormDisplay();
-                });
-
-                $(".field > input").on("blur", function(){
-                    adjustFormDisplay();
-                });
 			});
 		}
 	};
--- app/scripts/views/signupform.js
+++ app/scripts/views/signupform.js
@@ -1,6 +1,49 @@
-define(["jquery","underscore","backbone"],function($,_,Backbone){
+define(["jquery","underscore","backbone","validation"],function($,_,Backbone){
 
-	var SignupForm = Backbone.View.extend({
+
+    var ProgressiveForm = Backbone.View.extend({
+        initialize : function(){
+
+            console.log("calling progressive form");
+
+            this.$(".field > input").on("focus", _.bind(function(){
+                this.adjustFormDisplay();
+            },this));
+
+            this.$(".field > input").on("blur", _.bind(function(){
+                this.adjustFormDisplay();
+            },this));
+        },
+
+        adjustFormDisplay : function(){
+            if(this.$(".field > input").toArray().indexOf(document.activeElement) !== -1){
+                this.$(".field > .signup-button").removeClass("hidden-button");
+                this.$(".field > .domain-name").removeClass("hidden-text-box");
+                $(".aternative-login").addClass("collapsed");
+            } else {
+                this.$(".field > .signup-button").addClass("hidden-button");
+                this.$(".field > .domain-name").addClass("hidden-text-box");
+                $(".aternative-login").removeClass("collapsed");
+            }
+        }
+    });
+
+
+
+
+
+	var SignupForm = ProgressiveForm.extend({
+        initialize : function(arguments){
+
+            ProgressiveForm.prototype.initialize.apply(this,arguments);
+
+            $(this.el).validate({
+                submitHandler: function(form) {
+                    alert("submitting");
+                    //form.submit();
+                }
+            });
+        }
 	});
 
 	return {
@@ -10,7 +53,7 @@ define(["jquery","underscore","backbone"],function($,_,Backbone){
 			}
 
 			return new SignupForm({ el : form });
-		}	
+		}
 	};
 
 });
