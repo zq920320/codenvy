6fc3e8dd5ba0e1267010c3c6145185d3853710ae
callmephilip <philip.nuzhnyy@gmail.com>
2013-02-04 18:10:22 -0800

    only lint home made stuff - no linting for vendors

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,9 @@ module.exports = function( grunt ) {
     lint: {
       files: [
         'Gruntfile.js',
-        'app/scripts/**/*.js',
+        'app/scripts/*.js',
+        'app/scripts/models/**/*.js',
+        'app/scripts/views/**/*.js',
         'spec/**/*.js'
       ]
     },
