18c6591c0dd27c1d5e949cebbb6712c9baf65254
callmephilip <philip.nuzhnyy@gmail.com>
2013-02-04 17:58:05 -0800

    lint forgot password form module

--- app/scripts/views/forgotpasswordform.js
+++ app/scripts/views/forgotpasswordform.js
@@ -1,41 +1,41 @@
-define(["underscore","backbone","views/accountformbase","models/account"], 
+define(["underscore","views/accountformbase","models/account"], 
 
-	function(_,Backbone,AccountFormBase,Account){
+    function(_,AccountFormBase,Account){
 
-		var ForgotPasswordForm = AccountFormBase.extend({
-			__submit : function(form){
-				this.__showProgress();
-				this.trigger("submitting");
+        var ForgotPasswordForm = AccountFormBase.extend({
+            __submit : function(){
+                this.__showProgress();
+                this.trigger("submitting");
 
-				Account.recoverPassword(
-					this.$("input[name='email']").val(),
-					_.bind(function(d){
-						this.trigger("success",d);
-					},this),
-					_.bind(function(errors){
-						if(errors.length > 0){
-							this.trigger(
-								"invalid",
-								errors[0].getFieldName(),
-								errors[0].getErrorDescription()
-							);
-						}
-					},this)
-				);
-			}
-		});
+                Account.recoverPassword(
+                    this.$("input[name='email']").val(),
+                    _.bind(function(d){
+                        this.trigger("success",d);
+                    },this),
+                    _.bind(function(errors){
+                        if(errors.length > 0){
+                            this.trigger(
+                                "invalid",
+                                errors[0].getFieldName(),
+                                errors[0].getErrorDescription()
+                            );
+                        }
+                    },this)
+                );
+            }
+        });
 
-		return {
-			get : function(form){
-				if(typeof form === 'undefined'){
-					throw new Error("Need a form");
-				}
+        return {
+            get : function(form){
+                if(typeof form === 'undefined'){
+                    throw new Error("Need a form");
+                }
 
-				return new ForgotPasswordForm({ el : form });
-			},
+                return new ForgotPasswordForm({ el : form });
+            },
 
-			ForgotPasswordForm : ForgotPasswordForm
-		};
+            ForgotPasswordForm : ForgotPasswordForm
+        };
 
-	}
+    }
 );
