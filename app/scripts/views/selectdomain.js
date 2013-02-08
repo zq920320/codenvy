7242e1f972a6d8f8dee6ccf56bc3d80eca95b295
Mark Downey <mark.downey@exoplatform.com>
2013-02-07 16:12:38 -0800

    started domain selector view

---
+++ app/scripts/views/selectdomain.js
@@ -0,0 +1,17 @@
+define(["jquery","underscore","models/account", "backbone"],
+
+    function($,_,Account, Backbone){
+
+        var DomainSelector = Backbone.View.extend({
+
+            initialize : function(){
+                    
+            },
+
+        });
+
+        return DomainSelector;
+          
+
+    }
+);
