b1d1a1f85733f2ade8caf47f57a6ebedbd7e4212
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-02-10 15:14:07 -0800

    add application configuration module

---
+++ test/spec/config.spec.js
@@ -0,0 +1,13 @@
+define(["config"],function(Config){
+
+	describe("Application Configuration", function(){
+
+		describe("module", function(){
+			it("can be imported", function(){
+				expect(Config).to.be.ok;
+			});
+		});
+
+	});
+
+});
