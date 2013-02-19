a0ed749af4d5ddcfc166ca1616cc619306096119
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-02-19 07:34:09 -0800

    add suggested distribution directory structure

---
+++ dist/stage/scripts/views/signinform.js
@@ -0,0 +1,64 @@
+define(["jquery", "underscore", "views/accountformbase", "models/account"],
+
+    function($,_,AccountFormBase,Account){
+
+        var SignInForm = AccountFormBase.extend({
+
+            initialize : function(attributes){
+                AccountFormBase.prototype.initialize.apply(this,attributes);
+            },
+
+            __validationRules : function(){
+                return {
+                    password : {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                };
+            },
+
+            __submit : function(form){
+                Account.login(
+                    this.$("input[name='email']").val(),
+                    this.$("input[name='password']").val(),
+                    _.bind(function(data){
+                        $(this.el).attr('action',data.loginUrl);
+                        form.submit();
+                    },this),
+                    _.bind(function(errors){
+
+                        this.__restoreForm();
+
+                        if(errors.length !== 0){
+                            this.trigger(
+                                "invalid",
+                                errors[0].getFieldName(),
+                                errors[0].getErrorDescription()
+                            );
+                        }
+                    },this)
+                );
+
+                return false;
+            }
+
+        });
+
+        return {
+            get : function(form){
+                if(typeof form === 'undefined'){
+                    throw new Error("Need a form");
+                }
+
+                return new SignInForm({
+                    el : form
+                });
+            },
+
+            SignInForm : SignInForm
+        };
+    }
+);
