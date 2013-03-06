414418fc25e6a61fec6ad52b46651fe48b76b5b1
callmephilip <philip.nuzhnyy@gmail.com>
2013-03-05 21:37:44 -0800

    fix auto reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function( grunt ) {
       dist: {
         // http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
         options: {
-          css_dir: 'app/_site/styles',
+          css_dir: 'app/styles',
           sass_dir: 'app/styles',
           images_dir: 'app/images',
           javascripts_dir: 'temp/scripts',
