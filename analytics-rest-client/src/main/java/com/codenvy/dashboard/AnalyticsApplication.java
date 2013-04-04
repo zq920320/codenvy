24b498c352c08261f4b12a8a26f7100254313c76
Anatoliy Bazko <abazko@codenvy.com>
2013-04-04 15:23:35 +0300

    DASHB-5: timeline view implementation

---
+++ analytics-rest-client/src/main/java/com/codenvy/dashboard/AnalyticsApplication.java
@@ -0,0 +1,54 @@
+/*
+ *    Copyright (C) 2013 eXo Platform SAS.
+ *
+ *    This is free software; you can redistribute it and/or modify it
+ *    under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation; either version 2.1 of
+ *    the License, or (at your option) any later version.
+ *
+ *    This software is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this software; if not, write to the Free
+ *    Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *    02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.codenvy.dashboard;
+
+import com.codenvy.dashboard.scripts.ScriptService;
+
+import javax.ws.rs.core.Application;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/** @author <a href="mailto:abazko@codenvy.com">Anatoliy Bazko</a> */
+public class AnalyticsApplication extends Application {
+    private final Set<Object> singletons;
+
+    private final Set<Class<?>> classes;
+
+    /** {@link AnalyticsApplication} constructor. */
+    public AnalyticsApplication() {
+        singletons = new HashSet<Object>();
+        singletons.add(ScriptService.class);
+
+        classes = new HashSet<Class<?>>();
+        classes.add(ScriptService.class);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Object> getSingletons() {
+        return Collections.emptySet();
+    }
+}
