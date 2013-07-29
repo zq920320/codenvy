267cedce8485c240f906ae49e855a6cbb9a490a6
Dmitry Kuleshov <dkuleshov@codenvy.com>
2013-07-29 15:14:10 +0300

    DASHB-110: added correct file headers

--- analytics-core/src/main/java/com/codenvy/analytics/metrics/MessageTransformer.java
+++ analytics-core/src/main/java/com/codenvy/analytics/metrics/MessageTransformer.java
@@ -1,7 +1,21 @@
 /*
- *    Copyright (C) 2013 Codenvy.
  *
+ * CODENVY CONFIDENTIAL
+ * ________________
+ *
+ * [2012] - [2013] Codenvy, S.A.
+ * All Rights Reserved.
+ * NOTICE: All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any. The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
  */
+
 package com.codenvy.analytics.metrics;
 
 import java.util.ArrayList;
@@ -37,7 +51,7 @@ public class MessageTransformer {
             builder.append("#");
         }
 
-        List<String> result = new ArrayList<>(2);
+        List<String> result = new ArrayList<String>(2);
         result.add(dt);
         result.add(builder.toString());
 
