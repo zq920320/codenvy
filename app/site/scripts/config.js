213ea105f31b448c098434c5323f1d2ec8c1406d
Max Shaposhnik <mshaposhnik@codenvy.com>
2013-11-08 10:06:44 +0200

    CLDIDE-1367; remove error messages in query param;

--- app/site/scripts/config.js
+++ app/site/scripts/config.js
@@ -25,7 +25,8 @@ define(function(){
         // e.g. codenvy.con/contact?o=pricing-page
         originTrackerUrlParameterName : "o",
 
-        placeholderPolyfillUrl : "site/scripts/vendor/jquery.placeholder.min.js"
+        placeholderPolyfillUrl : "site/scripts/vendor/jquery.placeholder.min.js",
+        
 
 	};
 
