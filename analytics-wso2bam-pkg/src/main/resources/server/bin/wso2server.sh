a108334cb3f89d2a25ca16045fc808d840f5e3e7
Vladimir Bezpalchuk <bezpalchukv@gmail.com>
2013-11-11 15:58:10 +0200

    DASHB-188: Added ability to send errors from WSo2 server by email using SMTPAppender

--- analytics-wso2bam-pkg/src/main/resources/server/bin/wso2server.sh
+++ analytics-wso2bam-pkg/src/main/resources/server/bin/wso2server.sh
@@ -267,6 +267,8 @@ do
     -XX:HeapDumpPath="$CARBON_HOME/repository/logs/heap-dump.hprof" \
     -javaagent:"$CARBON_HOME/repository/components/plugins/jamm_0.2.5.wso2v2.jar" \
     $JAVA_OPTS \
+    -Dmail.smtp.socketFactory.class=javax.net.ssl.SSLSocketFactory \
+    -Dmail.smtp.socketFactory.port=465 \
     -Danalytics.bin.dir=$CARBON_HOME/bin \
     -Danalytics.scripts.dir=$CARBON_HOME/pigscripts \
     -Danalytics.configuration.dir=$CARBON_HOME/repository/conf \
