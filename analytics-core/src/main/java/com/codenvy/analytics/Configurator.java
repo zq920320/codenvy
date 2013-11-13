92cbd6e779a3993a1053d17e9b6da47c540fb589
Anatoliy Bazko <abazko@codenvy.com>
2013-11-13 13:56:28 +0000

    DASHB-198: Assembly for local usage

--- analytics-core/src/main/java/com/codenvy/analytics/Configurator.java
+++ analytics-core/src/main/java/com/codenvy/analytics/Configurator.java
@@ -43,6 +43,12 @@ public class Configurator {
     private static Properties properties = new Properties();
 
     static {
+        if (System.getenv().containsKey(CODENVY_LOCAL_CONF_DIR)) {
+            ANALYTICS_CONF_DIR = System.getenv(CODENVY_LOCAL_CONF_DIR);
+        } else {
+            ANALYTICS_CONF_DIR = System.getProperty("analytics.conf.dir");
+        }
+
         try {
             loadFromResource();
         } catch (IOException e) {
@@ -52,12 +58,6 @@ public class Configurator {
                 throw new IllegalStateException(
                         "Configurator can't be instantiated. There is no configuration to read from");
             }
-        }
-
-        if (System.getenv().containsKey(CODENVY_LOCAL_CONF_DIR)) {
-            ANALYTICS_CONF_DIR = System.getenv(CODENVY_LOCAL_CONF_DIR);
-        } else {
-            ANALYTICS_CONF_DIR = System.getProperty("analytics.conf.dir");
         }
     }
 
