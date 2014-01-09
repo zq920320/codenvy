2dbb4135271913823d7d8606da0523bc7c069f2e
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2014-01-09 13:32:42 +0200

    CLDIDE-1498: Check project name restrictions during creation and accepting factories

--- codenvy-factory-services/src/test/java/com/codenvy/factory/SimpleFactoryUrlFormatTest.java
+++ codenvy-factory-services/src/test/java/com/codenvy/factory/SimpleFactoryUrlFormatTest.java
@@ -92,8 +92,6 @@ public class SimpleFactoryUrlFormatTest {
                               {"http://codenvy.com/factory?v=1.0&vcs=git&idcommit=1234567&pname=eee&wname=ttt"}, // vcsurl par is missing
                               {"http://codenvy.com/factory?v=1.0&vcs=git&idcommit=1234567&pname=eee&wname=ttt&vcsurl=" +
                                enc(VALID_REPOSITORY_URL) + "&vcsurl=" + enc(VALID_REPOSITORY_URL)}, // vcsurl par is duplicated
-                              {"http://codenvy.com/factory?v=1.0&vcs=git&idcommit=1234567&pname=_eee&wname=ttt&vcsurl=" +
-                               enc(VALID_REPOSITORY_URL)},
                               {"http://codenvy.com/factory?v=1.0&vcs=git&idcommit=1234567&pname=.eee&wname=ttt&vcsurl=" +
                                enc(VALID_REPOSITORY_URL)},
                               {"http://codenvy.com/factory?v=1.0&vcs=git&idcommit=1234567&pname=-eee&wname=ttt&vcsurl=" +
