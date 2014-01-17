f95b96b68014b9b9706e655c5ff083dc6d814669
Anatoliy Bazko <abazko@codenvy.com>
2014-01-17 13:47:52 +0000

    DASHB-288: Backport metrics from 1.x version

---
+++ analytics-core/src/main/java/com/codenvy/analytics/metrics/sessions/factory/AcceptedFactories.java
@@ -0,0 +1,34 @@
+/*
+ *
+ * CODENVY CONFIDENTIAL
+ * ________________
+ *
+ * [2012] - [2014] Codenvy, S.A.
+ * All Rights Reserved.
+ * NOTICE: All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any. The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.analytics.metrics.sessions.factory;
+
+import com.codenvy.analytics.metrics.AbstractCount;
+import com.codenvy.analytics.metrics.MetricType;
+
+/** @author Anatoliy Bazko */
+public class AcceptedFactories extends AbstractCount {
+
+    public AcceptedFactories() {
+        super(MetricType.ACCEPTED_FACTORIES, MetricType.ACCEPTED_FACTORIES_LIST);
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
