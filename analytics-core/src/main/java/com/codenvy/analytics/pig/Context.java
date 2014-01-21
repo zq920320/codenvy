804535e1a9178435c8bf62e5bb2f52e4929b1131
Anatoliy Bazko <abazko@codenvy.com>
2014-01-21 18:18:22 +0000

    Unmodifiable execution context

--- analytics-core/src/main/java/com/codenvy/analytics/pig/Context.java
+++ analytics-core/src/main/java/com/codenvy/analytics/pig/Context.java
@@ -17,12 +17,93 @@
  */
 package com.codenvy.analytics.pig;
 
-import java.util.HashMap;
+import com.codenvy.analytics.metrics.MetricFilter;
+import com.codenvy.analytics.metrics.Parameters;
 
-/** @author Anatoliy Bazko */
-public class Context extends HashMap<String, String> {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-    public static Context create() {
-        return new Context();
+/**
+ * Unmodifiable execution context.
+ *
+ * @author Anatoliy Bazko
+ */
+public class Context {
+    private Map<String, String> context;
+
+    private Context(Map<String, String> context) {
+        this.context = context;
+    }
+
+    public String get(Parameters key) {
+        return context.get(key.name());
+    }
+
+    public String get(MetricFilter key) {
+        return context.get(key.name());
+    }
+
+    public boolean exists(Parameters key) {
+        return context.containsKey(key.name());
+    }
+
+    public boolean exists(MetricFilter key) {
+        return context.containsKey(key.name());
+    }
+
+    public Context cloneAndRemove(Parameters param) {
+        Builder builder = new Builder();
+        builder.putAll(context);
+        builder.remove(param);
+
+        return builder.build();
+    }
+
+    public Context cloneAndPut(Parameters param, String value) {
+        Builder builder = new Builder();
+        builder.putAll(context);
+        builder.put(param, value);
+
+        return builder.build();
+    }
+
+    public Context clone() {
+        Builder builder = new Builder();
+        builder.putAll(context);
+
+        return builder.build();
+    }
+
+    public static class Builder {
+        private Map<String, String> context = new LinkedHashMap<>();
+
+        public Builder put(Parameters param, String value) {
+            context.put(param.name(), value);
+            return this;
+        }
+
+        public Builder putDefaultValue(Parameters param) {
+            context.put(param.name(), param.getDefaultValue());
+            return this;
+        }
+
+        public Builder remove(Parameters param) {
+            context.remove(param.name());
+            return this;
+        }
+
+        public Builder put(MetricFilter param, String value) {
+            context.put(param.name(), value);
+            return this;
+        }
+
+        public Builder putAll(Map<String, String> context) {
+            context.putAll(context);
+            return this;
+        }
+
+        public Context build() {
+            return new Context(context);
+        }
     }
 }
