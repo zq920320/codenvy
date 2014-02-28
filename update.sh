07f58593ed70f369b6816cd032a50a4f10f2583b
Anatoliy Bazko <abazko@codenvy.com>
2014-02-28 12:19:05 +0000

    DASHB-287: Use pure Tomcat server instead of BAM

--- update.sh
+++ update.sh
@@ -1,3 +1,20 @@
+#
+# CODENVY CONFIDENTIAL
+# ________________
+#
+# [2012] - [2013] Codenvy, S.A.
+# All Rights Reserved.
+# NOTICE: All information contained herein is, and remains
+# the property of Codenvy S.A. and its suppliers,
+# if any. The intellectual and technical concepts contained
+# herein are proprietary to Codenvy S.A.
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Codenvy S.A..
+#
+
 #!/bin/bash
 filename=`ls analytics-tomcat-pkg/target | grep analytics-tomcat`
 SSH_KEY_NAME=cl-server-prod-20130219
@@ -12,9 +29,9 @@ deleteFileIfExists() {
     fi
 }
 
-    echo "==== Step [1/7] =======================> [upload a new Tomcat]"
+    echo "==== Step [1/7] =======================> [Uploading a new Tomcat]"
     scp -i ~/.ssh/${SSH_KEY_NAME} analytics-tomcat-pkg/target/${filename} ${SSH_AS_USER_NAME}@${AS_IP}:${filename}
-    echo "==== Step [2/7] =======================> [stop Tomcat]"
+    echo "==== Step [2/7] =======================> [Stoping Tomcat]"
     ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cp ${home}/wso2carbon.pid ${home}/pid"
     ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cd ${home}/bin/;if [ -f wso2server.sh ]; then ./wso2server.sh stop; fi"
 
@@ -25,18 +42,18 @@ deleteFileIfExists() {
         ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "ps x | grep `ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} cat ${home}/pid` > ${home}/wso2carbon.state"
         scp -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP}:${home}/wso2carbon.state ${testfile}
         if [ "`cat ${testfile} | wc -l`" == "2" ]; then
-            echo "==== Step [3/7] =======================> [Tomcat of application server has stopped]"
+            echo "==== Step [3/7] =======================> [Tomcat is stopped]"
             AS_STATE=Stopped
         fi
 
         sleep 5
     done
 
-    echo "==== Step [4/7] =======================> [clean up]"
+    echo "==== Step [4/7] =======================> [Cleaning up]"
     ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "rm -rf ${home}"
-    echo "==== Step [5/7] =======================> [unpack new tomcat]"
+    echo "==== Step [5/7] =======================> [Unpacking resources]"
     ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "unzip ${filename} -d wso2bam-analytics"
-    echo "==== Step [6/7] =======================> [start new Tomcat on ${AS_IP}]"
+    echo "==== Step [6/7] =======================> [Starting up on ${AS_IP}]"
     ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cd ${home}/bin;./wso2server.sh start"
 
     AS_STATE='Starting'
@@ -49,10 +66,9 @@ deleteFileIfExists() {
 
       if grep -Fq "WSO2 Carbon started in" ${testfile}
         then
-         echo "==== Step [7/7] ======================> [Tomcat of application server has started]"
+         echo "==== Step [7/7] ======================> [Analytics is started]"
          AS_STATE=Started
       fi
-
          sleep 5
     done
     echo ""
