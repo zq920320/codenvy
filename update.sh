bb46934cfc36a2a3735a23547e5f44f7106e1c97
Anatoliy Bazko <abazko@codenvy.com>
2014-03-02 15:14:34 +0000

    Release 2.2.0

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@
 #
 
 #!/bin/bash
-filename=`ls analytics-tomcat-pkg/target | grep analytics-tomcat`
+filename=`ls analytics-tomcat-pkg/target | grep analytics-tomcat-pkg`
 SSH_KEY_NAME=cl-server-prod-20130219
 SSH_AS_USER_NAME=logreader
 AS_IP=syslog.codenvycorp.com
@@ -32,39 +32,25 @@ deleteFileIfExists() {
     echo "==== Step [1/7] =======================> [Uploading a new Tomcat]"
     scp -i ~/.ssh/${SSH_KEY_NAME} analytics-tomcat-pkg/target/${filename} ${SSH_AS_USER_NAME}@${AS_IP}:${filename}
     echo "==== Step [2/7] =======================> [Stoping Tomcat]"
-    ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cp ${home}/wso2carbon.pid ${home}/pid"
-    ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cd ${home}/bin/;if [ -f wso2server.sh ]; then ./wso2server.sh stop; fi"
-
-    AS_STATE='Running'
-    testfile=/tmp/wso2carbon.state
-    while [ "${AS_STATE}" != "Stopped" ]; do
-        deleteFileIfExists ${testfile}
-        ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "ps x | grep `ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} cat ${home}/pid` > ${home}/wso2carbon.state"
-        scp -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP}:${home}/wso2carbon.state ${testfile}
-        if [ "`cat ${testfile} | wc -l`" == "2" ]; then
-            echo "==== Step [3/7] =======================> [Tomcat is stopped]"
-            AS_STATE=Stopped
-        fi
-
-        sleep 5
-    done
+    ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cd ${home}/bin/;if [ -f catalina.sh ]; then ./catalina.sh stop; fi"
+    echo "==== Step [3/7] =======================> [Server is stopped]"
 
     echo "==== Step [4/7] =======================> [Cleaning up]"
     ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "rm -rf ${home}"
     echo "==== Step [5/7] =======================> [Unpacking resources]"
-    ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "unzip ${filename} -d wso2bam-analytics"
+    ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "unzip ${filename} -d analytics-tomcat"
     echo "==== Step [6/7] =======================> [Starting up on ${AS_IP}]"
-    ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cd ${home}/bin;./wso2server.sh start"
+    ssh -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP} "cd ${home}/bin;./catalina.sh start"
 
     AS_STATE='Starting'
-    testfile=/tmp/wso2carbon.log
+    testfile=/tmp/catalina.log
     while [ "${AS_STATE}" != "Started" ]; do
 
     deleteFileIfExists ${testfile}
 
-    scp -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP}:${home}/repository/logs/wso2carbon.log ${testfile}
+    scp -i ~/.ssh/${SSH_KEY_NAME} ${SSH_AS_USER_NAME}@${AS_IP}:${home}/logs/catalina.out ${testfile}
 
-      if grep -Fq "WSO2 Carbon started in" ${testfile}
+      if grep -Fq "Server startup in" ${testfile}
         then
          echo "==== Step [7/7] ======================> [Analytics is started]"
          AS_STATE=Started
