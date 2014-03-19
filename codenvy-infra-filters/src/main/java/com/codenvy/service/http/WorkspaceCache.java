40da2aa771ca76ea256eca6467683814436219e5
Sergey Kabashnyuk <skabashnyuk@codenvy.com>
2014-03-19 16:15:18 +0200

    Moved filters from cloud-ide

---
+++ codenvy-infra-filters/src/main/java/com/codenvy/service/http/WorkspaceCache.java
@@ -0,0 +1,42 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2013] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.cloudide;
+
+import com.codenvy.api.workspace.shared.dto.Workspace;
+import com.codenvy.commons.lang.ExpirableCache;
+
+
+/**
+ * Cache that maps workspace to some information of that ws.
+ * Extends expirable cache to provide thread-safety.
+ */
+public class WorkspaceCache extends ExpirableCache<String, Workspace> {
+    public WorkspaceCache(long expiredAfter, int cacheSize) {
+        super(expiredAfter, cacheSize);
+    }
+
+    @Override
+    public synchronized void put(String key, Workspace value) {
+        super.put(key, value);
+    }
+
+    @Override
+    public synchronized Workspace get(String key) {
+        return super.get(key);
+    }
+}
---
+++ codenvy-infra-filters/src/main/java/com/codenvy/service/http/WorkspaceIdEnvironmentInitializationFilter.java
@@ -0,0 +1,80 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ * 
+ *  [2012] - [2013] Codenvy, S.A. 
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.service.http;
+
+import com.codenvy.api.workspace.server.dao.WorkspaceDao;
+import com.codenvy.api.workspace.server.exception.WorkspaceException;
+import com.codenvy.api.workspace.server.exception.WorkspaceNotFoundException;
+import com.codenvy.api.workspace.shared.dto.Workspace;
+import com.codenvy.commons.env.EnvironmentContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Alexander Garagatyi
+ */
+@Singleton
+public class WorkspaceIdEnvironmentInitializationFilter implements Filter {
+    private static final Logger LOG = LoggerFactory.getLogger(WorkspaceIdEnvironmentInitializationFilter.class);
+
+    @Inject
+    private WorkspaceDao workspaceDao;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        String requestUrl = httpRequest.getRequestURI();
+
+        String[] pathParts = requestUrl.split("/", 5);
+
+        try {
+            Workspace workspace = workspaceDao.getById(pathParts[3]);
+            final EnvironmentContext env = EnvironmentContext.getCurrent();
+            env.setWorkspaceName(workspace.getName());
+            env.setWorkspaceId(workspace.getId());
+            env.setAccountId(workspace.getOrganizationId());
+
+            chain.doFilter(request, response);
+        } catch (WorkspaceNotFoundException e) {
+            ((HttpServletResponse)response).sendError(HttpServletResponse.SC_NOT_FOUND,
+                                                      "Workspace with id " + pathParts[3] + " is not found");
+            return;
+        } catch (WorkspaceException e) {
+            throw new ServletException(e.getLocalizedMessage(), e);
+        } finally {
+            EnvironmentContext.reset();
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
