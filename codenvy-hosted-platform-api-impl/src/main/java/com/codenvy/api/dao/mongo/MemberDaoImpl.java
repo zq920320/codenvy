ce4827f264b373df78adb75d568a750e3a45ee1b
Eugene Voevodin <evoevodin@codenvy.com>
2014-04-29 11:20:07 +0300

    fixed order

--- codenvy-hosted-platform-api-impl/src/main/java/com/codenvy/api/dao/mongo/MemberDaoImpl.java
+++ codenvy-hosted-platform-api-impl/src/main/java/com/codenvy/api/dao/mongo/MemberDaoImpl.java
@@ -210,10 +210,10 @@ public class MemberDaoImpl implements MemberDao {
         DBObject query = new BasicDBObject("_id", member.getUserId());
         try {
             DBObject old = collection.findOne(query);
-            BasicDBList members = (BasicDBList)old.get("members");
             if (old == null) {
                 throw new NotFoundException(String.format("Workspace member with id %s doesn't exist", member.getUserId()));
             }
+            BasicDBList members = (BasicDBList)old.get("members");
             Iterator it = members.iterator();
             while (it.hasNext()) {
                 Member one = DtoFactory.getInstance().createDtoFromJson(it.next().toString(), Member.class);
