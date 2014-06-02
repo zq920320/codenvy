0589ae4b1888620656e4388c74c3c48460e6d21a
Sergey Kabashnyuk <skabashnyuk@codenvy.com>
2014-06-02 16:23:22 +0300

    Update Codenvy license

--- codenvy-hosted-infra-filters/src/main/java/com/codenvy/service/http/WorkspaceInfoCache.java
+++ codenvy-hosted-infra-filters/src/main/java/com/codenvy/service/http/WorkspaceInfoCache.java
@@ -1,10 +1,10 @@
 /*
  * CODENVY CONFIDENTIAL
  * __________________
- * 
- *  [2012] - [2014] Codenvy, S.A. 
+ *
+ *  [2012] - [2014] Codenvy, S.A.
  *  All Rights Reserved.
- * 
+ *
  * NOTICE:  All information contained herein is, and remains
  * the property of Codenvy S.A. and its suppliers,
  * if any.  The intellectual and technical concepts contained
@@ -12,9 +12,10 @@
  * and its suppliers and may be covered by U.S. and Foreign Patents,
  * patents in process, and are protected by trade secret or copyright law.
  * Dissemination of this information or reproduction of this material
-     * is strictly forbidden unless prior written permission is obtained
+ * is strictly forbidden unless prior written permission is obtained
  * from Codenvy S.A..
  */
+
 package com.codenvy.service.http;
 
 import com.codenvy.api.core.NotFoundException;
