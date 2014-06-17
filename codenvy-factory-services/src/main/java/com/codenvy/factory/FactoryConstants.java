d9c75f119b8aa790ff1cb0a22ab426278234e6e1
Max Shaposhnik <mshaposhnik@codenvy.com>
2014-06-17 14:40:59 +0300

    Message revert;

--- codenvy-factory-services/src/main/java/com/codenvy/factory/FactoryConstants.java
+++ codenvy-factory-services/src/main/java/com/codenvy/factory/FactoryConstants.java
@@ -32,11 +32,9 @@ public class FactoryConstants {
             "policy. Please contact the owner of this Factory for more information.";
 
     public static final String PARAMETRIZED_ILLEGAL_ORGID_PARAMETER_MESSAGE =
-            "You do not have a valid orgID. Your Factory configuration has a parameter that can only " +
-            "be used with a Tracked Factory subscription. You could have provided the wrong value, " +
-            "your subscription has expired, or you do not have a valid subscription account.  Please contact " +
-            "info@codenvy.com with any questions.\n  orgID Submitted: %s \n" +
-            "Invalid Parameter Name: Restriction ";
+            "You have provided an invalid orgId %s. You could have provided the wrong code, " +
+            "your subscription has expired, or you do not have a valid subscription account. Please contact " +
+            "info@codenvy.com with any questions.";
 
     public static final String PARAMETRIZED_ILLEGAL_TRACKED_PARAMETER_MESSAGE =
             "You have provided a Tracked Factory parameter %s, and you do not have a valid orgId %s. You could have " +
