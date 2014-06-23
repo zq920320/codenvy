401d3fb9ae04f233154a041804f5a1745169ad60
Max Shaposhnik <mshaposhnik@codenvy.com>
2014-06-23 14:05:42 +0300

    Fix migration service;

--- codenvy-hosted-ide3-migration-service/src/main/java/com/codenvy/migration/MigrationService.java
+++ codenvy-hosted-ide3-migration-service/src/main/java/com/codenvy/migration/MigrationService.java
@@ -5,17 +5,12 @@ import com.codenvy.migration.model.ProjectDescription;
 import com.codenvy.migration.model.Property;
 import com.codenvy.migration.model.ProjectTypes;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.util.*;
 
@@ -31,75 +26,71 @@ public class MigrationService {
 
 
     @GET
-    @RolesAllowed("system/admin")
+//    @RolesAllowed("system/admin")
     @Produces("text/html")
     public Response convert(@PathParam("ws-id") String workspaceId, @QueryParam("project") String projectName) {
 
         FileMetadataSerializer metadataSerializer = new FileMetadataSerializer();
 
         File wsFolder = PathCalculator.calculateDirPath(new File(fsRoot), workspaceId);
+        File propsFolder = new File(wsFolder, ".vfs/props");
 
-        File[] projectsList = projectName == null ? wsFolder.listFiles(new FileFilter() {
+        File[] projectsList = projectName == null ? propsFolder.listFiles(new FileFilter() {
             @Override
             public boolean accept(File pathname) {
-                return pathname.isDirectory() && !pathname.getName().equals(".vfs");
+                return pathname.getName().endsWith("_props");
             }
         })
-                                                  : new File[]{new File(wsFolder, projectName)};
+                                                  : new File[]{new File(propsFolder, projectName + "_props")};
 
         StringBuilder out = new StringBuilder();
+        Map<String, String[]> inputProps = new HashMap<>();
 
-        for (File projectFolder : projectsList) {
+        for (File projectFile : projectsList) {
             // In case manually set project
-            if (!projectFolder.exists() || !projectFolder.isDirectory()) {
-                return Response.status(400).entity(String.format("Cannot find FS for project %s", projectFolder.getName())).build();
+            try  {
+                 inputProps = metadataSerializer.read(new DataInputStream(new FileInputStream(projectFile)));
+            } catch (FileNotFoundException e) {
+                return Response.status(400)
+                               .entity(String.format("Cannot find FS for project %s", projectFile.getName())).build();
+            }
+            catch (IOException e) {
+                return Response.status(500)
+                               .entity(String.format("IO Exception for project %s", projectFile.getName())).build();
             }
 
+            String projectType;
+            String currentName = projectFile.getName().substring(0, projectName.lastIndexOf("_props"));
+            List<Property> outputProps = new ArrayList<>();
 
-            String projectType = ProjectTypes.UNKNOWN.toString();
-            File[] list = projectFolder.listFiles();
-            if (list == null) {
-                out.append(String.format("Project %s FS is found, but it is empty. <br/> ", projectFolder.getName()));
-                continue;
-            }
 
-            List<Property> props = new ArrayList<>();
+            switch (inputProps.get("vfs:projectType")[0]) {
 
-            for (File one : list) {
-                if (one.getName().equals("pom.xml")) {
+                case "Jar": {
                     projectType = ProjectTypes.MAVEN.toString();
-                    props.add(new Property("builder.name", new String[]{"maven"}));
-
-                    // Trying to detect runner
-                    String packaging = null;
-                    try {
-                        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-                                .newInstance();
-                        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-                        Document document = documentBuilder.parse(one);
-                        packaging = document.getElementsByTagName("packaging").item(0).getTextContent();
-                    } catch (ParserConfigurationException  | SAXException  | IOException e) {
-                        out.append(String.format("Cannot find packaging for project %s <br/>", projectFolder.getName()));
-                    }
-                    if (packaging == null)
-                        break;
-                    if (packaging.equals("war"))
-                        props.add(new Property("runner.name", new String[] {"JavaWeb"}));
-                    else if (packaging.equals("jar"))
-                        props.add(new Property("runner.name", new String[] {"JavaStandalone"}));
+                    outputProps.add(new Property("builder.name", new String[]{"maven"}));
+                    outputProps.add(new Property("runner.name", new String[]{"JavaStandalone"}));
                     break;
                 }
-
+                case "Servlet/JSP": {
+                    projectType = ProjectTypes.MAVEN.toString();
+                    outputProps.add(new Property("builder.name", new String[]{"maven"}));
+                    outputProps.add(new Property("runner.name", new String[]{"JavaWeb"}));
+                    break;
+                }
+                default: {
+                    projectType = ProjectTypes.UNKNOWN.toString();
+                }
             }
 
 
             ProjectDescription projectDescription = new ProjectDescription();
             projectDescription.setType(projectType);
-            projectDescription.setProperties(props.toArray(new Property[props.size()]));
+            projectDescription.setProperties(outputProps.toArray(new Property[outputProps.size()]));
 
 
             // Writing collected data to a file
-            File codenvyFolder = new File(projectFolder, ".codenvy");
+            File codenvyFolder = new File(wsFolder, currentName + "/.codenvy");
             if (!codenvyFolder.exists())
                 codenvyFolder.mkdir();
 
@@ -107,15 +98,16 @@ public class MigrationService {
             File descriptionFile = new File(codenvyFolder, "project");
             if (!descriptionFile.exists()) {
                 try (Writer writer = new BufferedWriter(new FileWriter(descriptionFile))) {
-                    out.append(String.format("Converting project: %s, type: %s <br/> ", projectFolder.getName(),
+                    out.append(String.format("Converting project: %s, type: %s <br/> ", projectFile.getName(),
                                              projectDescription.getType()));
                     writer.write(JsonHelper.toJson(projectDescription));
-                    File metadataDir = new File(codenvyFolder, PROPS_DIR );
-                    File metadataFile= new File(metadataDir, descriptionFile.getName() + PROPERTIES_FILE_SUFFIX);
+                    File metadataDir = new File(codenvyFolder, PROPS_DIR);
+                    File metadataFile = new File(metadataDir, descriptionFile.getName() + PROPERTIES_FILE_SUFFIX);
                     metadataFile.getParentFile().mkdirs();
-                    DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(metadataFile)));
+                    DataOutputStream dos =
+                            new DataOutputStream(new BufferedOutputStream(new FileOutputStream(metadataFile)));
                     Map<String, String[]> properties = new HashMap<>();
-                    properties.put("vfs:mimeType",  new String[]{"application/json"});
+                    properties.put("vfs:mimeType", new String[]{"application/json"});
                     metadataSerializer.write(dos, properties);
                     if (dos != null)
                         dos.close();
