c7ecc78833e92f45527e937c241f698010f3612c
Max Shaposhnik <mshaposhnik@codenvy.com>
2014-08-01 12:56:53 +0300

    CLDIDE-1949; block IDE2 logins after given date;

---
+++ codenvy-hosted-infra-filters/src/main/java/com/codenvy/service/http/IdeVersionHolder.java
@@ -0,0 +1,39 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2014] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.service.http;
+
+/**
+ * Holder for version ThreadLocal
+ */
+public class IdeVersionHolder {
+
+    private static ThreadLocal<Boolean> isIDE2 = new ThreadLocal<Boolean>() {
+        @Override
+        protected Boolean initialValue() {
+            return Boolean.FALSE;
+        }
+    };
+
+    public static boolean get() {
+        return isIDE2.get().booleanValue();
+    }
+
+    public static void set(boolean value) {
+         isIDE2.set(Boolean.valueOf(value));
+    }
+}
---
+++ codenvy-hosted-infra-filters/src/main/java/com/codenvy/service/http/IdeVersionInitializationFilter.java
@@ -0,0 +1,52 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2014] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.service.http;
+
+import javax.inject.Singleton;
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Sets current IDE version to the thread local variable.
+ */
+@Singleton
+public class IdeVersionInitializationFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        try {
+            String host = servletRequest.getServerName();
+            if (host.toLowerCase().startsWith("c2.")) {
+                IdeVersionHolder.set(true);
+            }
+        } finally {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
