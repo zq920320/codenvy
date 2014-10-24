0260fc52193bf2defeaf07280d555a15e97dfa90
Viktor Kuznyetsov <vkuznyetsov@codenvy.com>
2014-10-24 11:54:54 +0300

    CLDIDE removed UV widget from all site pages

--- app/site/scripts/views/signupform.js
+++ app/site/scripts/views/signupform.js
@@ -78,7 +78,6 @@ define(["jquery","underscore","models/account","views/accountformbase","validati
 
             initialize : function(attributes){
                 ProgressiveForm.prototype.initialize.apply(this,attributes);
-                Account.supportTab();
                 //bind onclick to Google and GitHub buttons
                 $(".oauth-button.google").click(function(){
                     Account.loginWithGoogle("Main page", function(url){
