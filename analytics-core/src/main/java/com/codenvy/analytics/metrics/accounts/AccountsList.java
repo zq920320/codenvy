635c90d68344c2685509e203369da2aaaad22df7
Anatoliy Bazko <abazko@codenvy.com>
2014-11-06 17:28:43 +0200

    DASHB-572: "Can not get users in workspaces of current user" errors at the user dmitry.ndp@gmail.com statistics pages

--- analytics-core/src/main/java/com/codenvy/analytics/metrics/accounts/AccountsList.java
+++ analytics-core/src/main/java/com/codenvy/analytics/metrics/accounts/AccountsList.java
@@ -69,9 +69,12 @@ public class AccountsList extends AbstractAccountMetric {
                 m.put(ACCOUNT_ID, StringValueData.valueOf(accountMembership.getAccountReference().getId()));
                 m.put(ACCOUNT_NAME, StringValueData.valueOf(accountMembership.getAccountReference().getName()));
                 m.put(ACCOUNT_ROLES, StringValueData.valueOf(accountMembership.getRoles().toString()));
-                m.put(ACCOUNT_ATTRIBUTES, StringValueData.valueOf(
-                        getAccountDescriptorById(accountMembership.getAccountReference().getId())
-                                .getAttributes().toString()));
+                try {
+                    m.put(ACCOUNT_ATTRIBUTES, StringValueData.valueOf(getAccountDescriptorById(accountMembership.getAccountReference().getId())
+                                                                              .getAttributes().toString()));
+                } catch (IOException e) { // if use isn't owner of the account
+                    m.put(ACCOUNT_ATTRIBUTES, StringValueData.DEFAULT);
+                }
                 m.put(ACCOUNT_USER_ID, StringValueData.valueOf(accountMembership.getUserId()));
                 m.put(ACCOUNT_PROFILE_EMAIL, getEmail(profile));
                 m.put(ACCOUNT_PROFILE_NAME, getFullName(profile));
