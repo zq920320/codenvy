569009103b3382bc142fb46bf3210dd5e2974fb0
Alexander Garagatyi <agaragatyi@codenvy.com>
2015-01-16 09:20:26 +0200

    CLDIDE-2226: fix retrieving of paid accounts that should be charged if last account charge date is unset

--- codenvy-hosted-platform-api-impl/src/main/java/com/codenvy/api/dao/mongo/AccountDaoImpl.java
+++ codenvy-hosted-platform-api-impl/src/main/java/com/codenvy/api/dao/mongo/AccountDaoImpl.java
@@ -390,19 +390,6 @@ public class AccountDaoImpl implements AccountDao {
         }
     }
 
-//    @Override
-//    public List<Subscription> getSubscriptions(SubscriptionQueryBuilder queryBuilder) throws ServerException {
-//        try (DBCursor subscriptions = subscriptionCollection.find((DBObject)queryBuilder.build())) {
-//            final ArrayList<Subscription> result = new ArrayList<>();
-//            for (DBObject subscriptionObj : subscriptions) {
-//                result.add(toSubscription(subscriptionObj));
-//            }
-//            return result;
-//        } catch (MongoException me) {
-//            LOG.error(me.getMessage(), me);
-//            throw new ServerException("It is not possible to retrieve subscriptions");
-//        }
-//    }
     @Override
     public SubscriptionQueryBuilder getSubscriptionQueryBuilder() {
         return subscriptionQueryBuilder;
@@ -414,19 +401,33 @@ public class AccountDaoImpl implements AccountDao {
             throw new ForbiddenException("Date can't be null");
         }
 
-//        DBObject paidAccountQuery = new BasicDBObject("name", "codenvy:paid");
+        DBObject paidAccountQuery = new BasicDBObject("name", "codenvy:paid");
+        //
+        DBObject billingDatePropertyExist = new BasicDBObject("name", "codenvy:billing.date");
+        DBObject billingDatePropertyValueLessThanSpecified = new BasicDBObject("value", new BasicDBObject("$lt", String.valueOf(newDate.getTime())));
+        DBObject billingDateFits2 = new QueryBuilder().and(billingDatePropertyExist,
+                                                           billingDatePropertyValueLessThanSpecified).get();
+        DBObject billingDatePropertyDoesNotExist = QueryBuilder.start("attributes.name").notIn("codenvy:billing.date").get();
+        DBObject query2 = new QueryBuilder().and(paidAccountQuery,
+                                                 new QueryBuilder().or(billingDatePropertyDoesNotExist,
+                                                                       billingDateFits2).get()).get();
 
+                //
 //        DBObject billingDateFits = QueryBuilder.start().and(new BasicDBObject("name", "codenvy:billing.date"),
-//                                                            QueryBuilder.start("value").lessThan(String.valueOf(newDate.getTime())).get())
+//                                                            new BasicDBObject("value",
+//                                                                              new BasicDBObject("$lt", String.valueOf(newDate.getTime()))))
 //                                               .get();
 //
 //        DBObject query = QueryBuilder.start().and(QueryBuilder.start("attributes").elemMatch(paidAccountQuery).get(),
-//                                                  QueryBuilder.start("attributes").elemMatch(billingDateFits).get()).get();
+//                                                  QueryBuilder.start().or(
+//                                                          QueryBuilder.start("attributes").elemMatch(billingDateFits).get(),
+//                                                          QueryBuilder.start("attributes.name").notIn("codenvy:billing.date").get())
+//                                                              .get())
+//                                     .get();
         // TODO accounts with no codenvy:billing.date attributes should match
 
-        // TODO commented for tests and demo
-//        try (DBCursor accounts = accountCollection.find(query)) {
-        try (DBCursor accounts = accountCollection.find(new BasicDBObject("attributes.name", "codenvy:paid"))) {
+        try (DBCursor accounts = accountCollection.find(query2)) {
+//        try (DBCursor accounts = accountCollection.find(new BasicDBObject("attributes.name", "codenvy:paid"))) {
             final ArrayList<Account> result = new ArrayList<>();
             for (DBObject accountObj : accounts) {
                 result.add(toAccount(accountObj));
