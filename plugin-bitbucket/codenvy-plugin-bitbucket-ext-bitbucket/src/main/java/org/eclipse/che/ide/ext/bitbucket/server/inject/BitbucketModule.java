0729039cb9418b18dc11daf07de4e79a6bd043f6
Sergii Leschenko <sleschenko@codenvy.com>
2016-01-04 10:41:00 +0200

    IDEX-3920 Adapt source code to SSH API

--- plugin-bitbucket/codenvy-plugin-bitbucket-ext-bitbucket/src/main/java/org/eclipse/che/ide/ext/bitbucket/server/inject/BitbucketModule.java
+++ plugin-bitbucket/codenvy-plugin-bitbucket-ext-bitbucket/src/main/java/org/eclipse/che/ide/ext/bitbucket/server/inject/BitbucketModule.java
@@ -11,12 +11,12 @@
 package org.eclipse.che.ide.ext.bitbucket.server.inject;
 
 import org.eclipse.che.api.project.server.ProjectImporter;
+import org.eclipse.che.git.impl.nativegit.ssh.SshKeyUploader;
 import org.eclipse.che.ide.ext.bitbucket.server.Bitbucket;
 import org.eclipse.che.ide.ext.bitbucket.server.BitbucketKeyUploader;
 import org.eclipse.che.ide.ext.bitbucket.server.BitbucketProjectImporter;
 import org.eclipse.che.ide.ext.bitbucket.server.rest.BitbucketExceptionMapper;
 import org.eclipse.che.ide.ext.bitbucket.server.rest.BitbucketService;
-import org.eclipse.che.ide.ext.ssh.server.SshKeyUploader;
 import org.eclipse.che.inject.DynaModule;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
