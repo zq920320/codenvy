c818faed65a8994d9cc2c615c141e54aa55f26b7
Yevhenii Voevodin <evoevodin@codenvy.com>
2016-02-09 14:31:09 +0200

    CLDIDE-2692: Add resources limits checks

---
+++ codenvy-hosted-platform-api-impl/src/main/java/com/codenvy/api/workspace/LimitsCheckingWorkspaceConfigValidator.java
@@ -0,0 +1,67 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2016] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.api.workspace;
+
+import org.eclipse.che.api.core.BadRequestException;
+import org.eclipse.che.api.core.model.workspace.Environment;
+import org.eclipse.che.api.core.model.workspace.WorkspaceConfig;
+import org.eclipse.che.api.workspace.server.DefaultWorkspaceConfigValidator;
+import org.eclipse.che.commons.lang.Size;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import static java.lang.String.format;
+
+/**
+ * Checks workspace configuration object limitations,
+ * in the case of limitation violation throws {@link BadRequestException}.
+ *
+ * @author Yevhenii Voevodin
+ */
+@Singleton
+public class LimitsCheckingWorkspaceConfigValidator extends DefaultWorkspaceConfigValidator {
+
+    private final long maxRamPerWorkspaceEnv;
+
+    @Inject
+    public LimitsCheckingWorkspaceConfigValidator(@Named("limits.workspace.env.ram") String maxRam) {
+        maxRamPerWorkspaceEnv = Size.parseSizeToMegabytes(maxRam);
+    }
+
+    @Override
+    public void validate(WorkspaceConfig config) throws BadRequestException {
+        super.validate(config);
+        for (Environment environment : config.getEnvironments()) {
+            final long workspaceRam = environment.getMachineConfigs()
+                                                 .stream()
+                                                 .mapToInt(machineCfg -> machineCfg.getLimits().getRam())
+                                                 .sum();
+            if (workspaceRam > maxRamPerWorkspaceEnv) {
+                throw new BadRequestException(format("The environment '%s' of the workspace configuration '%s' "
+                                                     + "exceeds maximum available RAM per workspace. The maximum available value is '%dmb' "
+                                                     + "but the received value is '%dmb'.",
+                                                     environment.getName(),
+                                                     config.getName(),
+                                                     maxRamPerWorkspaceEnv,
+                                                     workspaceRam));
+            }
+        }
+    }
+}
---
+++ codenvy-hosted-platform-api-impl/src/test/java/com/codenvy/api/workspace/LimitsCheckingWorkspaceManagerTest.java
@@ -0,0 +1,111 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2016] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.api.workspace;
+
+import com.codenvy.api.workspace.LimitsCheckingWorkspaceManager.WorkspaceCallback;
+import com.google.common.collect.ImmutableList;
+
+import org.eclipse.che.api.core.BadRequestException;
+import org.eclipse.che.api.workspace.server.model.impl.UsersWorkspaceImpl;
+import org.testng.annotations.Test;
+
+import static com.codenvy.api.workspace.TestObjects.createConfig;
+import static com.codenvy.api.workspace.TestObjects.createRuntime;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Tests for {@link LimitsCheckingWorkspaceManager}.
+ *
+ * @author Yevhenii Voevodin
+ */
+public class LimitsCheckingWorkspaceManagerTest {
+
+    @Test(expectedExceptions = BadRequestException.class,
+          expectedExceptionsMessageRegExp = "The limit of the user's workspaces which is '2' is exceeded")
+    public void shouldNotBeAbleToCreateNewWorkspaceIfLimitIsExceeded() throws Exception {
+        final LimitsCheckingWorkspaceManager manager = spy(new LimitsCheckingWorkspaceManager(2, // <- workspaces max count
+                                                                                              "2gb",
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null));
+        doReturn(ImmutableList.of(mock(UsersWorkspaceImpl.class), mock(UsersWorkspaceImpl.class))) // <- currently used 2
+                .when(manager)
+                .getWorkspaces(anyString());
+
+        manager.checkCountAndPropagateCreation("user123", null);
+    }
+
+    @Test
+    public void shouldCallCreateCallBackIfEverythingIsOkayWithLimits() throws Exception {
+        final LimitsCheckingWorkspaceManager manager = spy(new LimitsCheckingWorkspaceManager(2, // <- workspaces max count
+                                                                                              "2gb",
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null));
+        doReturn(emptyList()).when(manager).getWorkspaces(anyString()); // <- currently used 0
+
+        final WorkspaceCallback callback = mock(WorkspaceCallback.class);
+        manager.checkCountAndPropagateCreation("user123", callback);
+
+        verify(callback).call();
+    }
+
+    @Test(expectedExceptions = BadRequestException.class,
+          expectedExceptionsMessageRegExp = "Could not start the workspace '.*', " +
+                                            "the maximum available RAM '2048mb' was exceeded, " +
+                                            "currently used RAM is '2048mb', wanted to allocated additional '1024mb'")
+    public void shouldNotBeAbleToStartNewWorkspaceIfRamLimitIsExceeded() throws Exception {
+        final LimitsCheckingWorkspaceManager manager = spy(new LimitsCheckingWorkspaceManager(2,
+                                                                                              "2gb", // <- workspaces ram limit
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null));
+        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getRuntimeWorkspaces(anyString()); // <- currently running 2gb
+
+        manager.checkRamAndPropagateStart(createConfig("1gb"), null, "user123", null);
+    }
+
+    @Test
+    public void shouldCallStartCallbackIfEverythingIsOkayWithLimits() throws Exception {
+        final LimitsCheckingWorkspaceManager manager = spy(new LimitsCheckingWorkspaceManager(2,
+                                                                                              "3gb", // <- workspaces ram limit
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null));
+        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getRuntimeWorkspaces(anyString()); // <- currently running 2gb
+
+        final WorkspaceCallback callback =  mock(WorkspaceCallback.class);
+        manager.checkRamAndPropagateStart(createConfig("1gb"), null, "user123", callback);
+
+        verify(callback).call();
+    }
+}
