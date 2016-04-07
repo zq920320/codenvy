2ec290a1df5707588310df8844fcbb3803638533
Dmytro Nochevnov <dnochevnov@codenvy.com>
2016-04-07 18:47:39 +0300

    Add hosts into the /etc/hosts of Codenvy on-prem from the new line

--- cdec/installation-manager-core/src/main/java/com/codenvy/im/artifacts/helper/CDECSingleServerHelper.java
+++ cdec/installation-manager-core/src/main/java/com/codenvy/im/artifacts/helper/CDECSingleServerHelper.java
@@ -104,10 +104,10 @@ public class CDECSingleServerHelper extends CDECArtifactHelper {
             case 1:
                 return new MacroCommand(new ArrayList<Command>() {{
                     add(createCommand("if ! sudo grep -Eq \"127.0.0.1.*puppet\" /etc/hosts; then\n" +
-                                      " echo '127.0.0.1 puppet' | sudo tee --append /etc/hosts > /dev/null\n" +
+                                      " echo '\n127.0.0.1 puppet' | sudo tee --append /etc/hosts > /dev/null\n" +
                                       "fi"));
                     add(createCommand(format("if ! sudo grep -Eq \" %1$s$\" /etc/hosts; then\n" +
-                                             "  echo \"127.0.0.1 %1$s\" | sudo tee --append /etc/hosts > /dev/null\n" +
+                                             "  echo \"\n127.0.0.1 %1$s\" | sudo tee --append /etc/hosts > /dev/null\n" +
                                              "fi", config.getHostUrl())));
 
                     // install puppet rpm
