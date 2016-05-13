124c8cc156d8b6dc9649e607ea1a11bc4f9a3a29
Sergii Leschenko <sleschenko@codenvy.com>
2016-05-13 10:21:13 +0300

    Remove deprecated code

--- assembly/onpremises-ide-packaging-war-platform-api/src/main/java/com/codenvy/api/deploy/OnPremisesIdeApiModule.java
+++ assembly/onpremises-ide-packaging-war-platform-api/src/main/java/com/codenvy/api/deploy/OnPremisesIdeApiModule.java
@@ -270,7 +270,6 @@ public class OnPremisesIdeApiModule extends AbstractModule {
         bind(ProfileMigrator.class).asEagerSingleton();
 
         install(new com.codenvy.workspace.interceptor.InterceptorModule());
-        install(new com.codenvy.auth.sso.server.deploy.SsoServerInterceptorModule());
         install(new com.codenvy.auth.sso.server.deploy.SsoServerModule());
 
         install(new InstrumentationModule());
