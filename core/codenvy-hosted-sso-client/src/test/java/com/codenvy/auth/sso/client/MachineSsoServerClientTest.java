5bd548aed17fc2fcdf03d2afd92c46bd59981f93
Anton Korneta <akorneta@codenvy.com>
2016-06-01 11:15:53 +0300

    CODENVY-495: Replaced api token to machine token in sso principal

--- core/codenvy-hosted-sso-client/src/test/java/com/codenvy/auth/sso/client/MachineSsoServerClientTest.java
+++ core/codenvy-hosted-sso-client/src/test/java/com/codenvy/auth/sso/client/MachineSsoServerClientTest.java
@@ -14,25 +14,19 @@
  */
 package com.codenvy.auth.sso.client;
 
-import com.codenvy.api.dao.authentication.AccessTicket;
-import com.codenvy.api.dao.authentication.TicketManager;
-import com.codenvy.auth.sso.server.ticket.InMemoryTicketManager;
 import com.codenvy.auth.sso.shared.dto.SubjectDto;
 import com.codenvy.machine.authentication.server.MachineTokenRegistry;
 
 import org.eclipse.che.api.core.NotFoundException;
-import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.core.rest.HttpJsonRequest;
 import org.eclipse.che.api.core.rest.HttpJsonRequestFactory;
 import org.eclipse.che.api.core.rest.HttpJsonResponse;
 import org.eclipse.che.api.user.server.UserManager;
 import org.eclipse.che.commons.subject.Subject;
-import org.eclipse.che.commons.subject.SubjectImpl;
 import org.eclipse.che.commons.test.SelfReturningAnswer;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static java.util.Collections.singleton;
 import static org.eclipse.che.dto.server.DtoFactory.newDto;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -56,20 +50,16 @@ public class MachineSsoServerClientTest {
     private static final String ENDPOINT = "http://localhost";
 
     private MachineTokenRegistry   registrySpy;
-    private HttpJsonResponse       responseMock;
     private UserManager            userManagerMock;
-    private TicketManager          ticketManager;
     private MachineSsoServerClient ssoClient;
 
     @BeforeMethod
     public void initClient() throws Exception {
-        ticketManager = new InMemoryTicketManager();
-
         registrySpy = spy(new MachineTokenRegistry());
 
         userManagerMock = mock(UserManager.class);
 
-        responseMock = mock(HttpJsonResponse.class);
+        final HttpJsonResponse responseMock = mock(HttpJsonResponse.class);
         when(responseMock.asDto(SubjectDto.class)).thenReturn(newDto(SubjectDto.class));
 
         final HttpJsonRequest requestMock = mock(HttpJsonRequest.class, new SelfReturningAnswer());
@@ -82,7 +72,6 @@ public class MachineSsoServerClientTest {
         ssoClient = new MachineSsoServerClient(ENDPOINT,
                                                requestFactoryMock,
                                                registrySpy,
-                                               ticketManager,
                                                userManagerMock);
     }
 
@@ -102,7 +91,7 @@ public class MachineSsoServerClientTest {
     }
 
     @Test
-    public void getUserMustReturnTheUserRetrievedFromTheApiRequest() throws NotFoundException, ServerException {
+    public void getSubjectMustReturnTheSubjectRetrievedFromTheApiRequestWithMachineToken() throws Exception {
         // machine token
         final String token = registrySpy.generateToken("user123", "workspace1234");
         // mocking the user descriptor which will be returned from the user api
@@ -111,10 +100,6 @@ public class MachineSsoServerClientTest {
                                                               .withEmail("mail")
                                                               .withId("user123");
         when(userManagerMock.getById(any())).thenReturn(user);
-        // creating the ticket for figuring out the master's token
-        ticketManager.putAccessTicket(new AccessTicket("master_token",
-                                                       new SubjectImpl("user", "user123", "master_token", singleton("user")),
-                                                       "auth_type"));
 
         final Subject sessionUser = ssoClient.getSubject(token, "client");
 
@@ -122,7 +107,7 @@ public class MachineSsoServerClientTest {
         assertEquals(sessionUser.getUserName(), user.getName());
         assertEquals(sessionUser.getUserId(), user.getId());
         assertFalse(sessionUser.isTemporary());
-        assertEquals(sessionUser.getToken(), "master_token");
+        assertEquals(sessionUser.getToken(), token);
         assertTrue(sessionUser.isMemberOf("user"));
     }
 }
