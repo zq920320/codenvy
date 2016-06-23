a0ffd29138d12f3415f901051d9f18b3e793b44b
Max Shaposhnik <mshaposhnik@codenvy.com>
2016-06-23 15:39:06 +0300

    Codenvy 694 Fix limits checking user on ws startup (#319)
    * CODENVY-694; check limits of user which namespace is used on ws startup

--- core/codenvy-hosted-platform-api-impl/src/test/java/com/codenvy/api/workspace/LimitsCheckingWorkspaceManagerTest.java
+++ core/codenvy-hosted-platform-api-impl/src/test/java/com/codenvy/api/workspace/LimitsCheckingWorkspaceManagerTest.java
@@ -17,8 +17,14 @@ package com.codenvy.api.workspace;
 import com.codenvy.api.workspace.LimitsCheckingWorkspaceManager.WorkspaceCallback;
 import com.google.common.collect.ImmutableList;
 
+import org.eclipse.che.api.core.NotFoundException;
+import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.core.model.workspace.WorkspaceConfig;
+import org.eclipse.che.api.user.server.UserManager;
+import org.eclipse.che.api.user.server.dao.User;
 import org.eclipse.che.api.workspace.server.model.impl.WorkspaceImpl;
+import org.mockito.ArgumentCaptor;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.codenvy.api.workspace.TestObjects.createConfig;
@@ -26,8 +32,11 @@ import static com.codenvy.api.workspace.TestObjects.createRuntime;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
@@ -56,7 +65,7 @@ public class LimitsCheckingWorkspaceManagerTest {
                                                                                               false));
         doReturn(ImmutableList.of(mock(WorkspaceImpl.class), mock(WorkspaceImpl.class))) // <- currently used 2
                                                                                          .when(manager)
-                                                                                         .getWorkspaces(anyString());
+                                                                                         .getByNamespace(anyString());
 
         manager.checkCountAndPropagateCreation("user123", null);
     }
@@ -75,7 +84,7 @@ public class LimitsCheckingWorkspaceManagerTest {
                                                                                               false));
         doReturn(ImmutableList.of(mock(WorkspaceImpl.class), mock(WorkspaceImpl.class))) // <- currently used 2
                                                                                          .when(manager)
-                                                                                         .getWorkspaces(anyString());
+                                                                                         .getByNamespace(anyString());
         final WorkspaceCallback callback = mock(WorkspaceCallback.class);
 
         manager.checkCountAndPropagateCreation("user123", callback);
@@ -97,7 +106,7 @@ public class LimitsCheckingWorkspaceManagerTest {
                                                                                               null,
                                                                                               false,
                                                                                               false));
-        doReturn(emptyList()).when(manager).getWorkspaces(anyString()); // <- currently used 0
+        doReturn(emptyList()).when(manager).getByNamespace(anyString()); // <- currently used 0
 
         final WorkspaceCallback callback = mock(WorkspaceCallback.class);
         manager.checkCountAndPropagateCreation("user123", callback);
@@ -119,7 +128,7 @@ public class LimitsCheckingWorkspaceManagerTest {
                                                                                               null,
                                                                                               false,
                                                                                               false));
-        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getWorkspaces(anyString()); // <- currently running 2gb
+        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getByNamespace(anyString()); // <- currently running 2gb
 
         manager.checkRamAndPropagateStart(createConfig("1gb"), null, "user123", null);
     }
@@ -136,7 +145,7 @@ public class LimitsCheckingWorkspaceManagerTest {
                                                                                               null,
                                                                                               false,
                                                                                               false));
-        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getWorkspaces(anyString()); // <- currently running 2gb
+        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getByNamespace(anyString()); // <- currently running 2gb
         final WorkspaceCallback callback = mock(WorkspaceCallback.class);
 
         manager.checkRamAndPropagateStart(createConfig("1gb"), null, "user123", callback);
@@ -158,7 +167,7 @@ public class LimitsCheckingWorkspaceManagerTest {
                                                                                               null,
                                                                                               false,
                                                                                               false));
-        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getWorkspaces(anyString()); // <- currently running 2gb
+        doReturn(singletonList(createRuntime("1gb", "1gb"))).when(manager).getByNamespace(anyString()); // <- currently running 2gb
 
         final WorkspaceCallback callback = mock(WorkspaceCallback.class);
         manager.checkRamAndPropagateStart(createConfig("1gb"), null, "user123", callback);
@@ -236,5 +245,58 @@ public class LimitsCheckingWorkspaceManagerTest {
                                                                                               false,
                                                                                               false));
         manager.checkMaxEnvironmentRam(config);
+    }
+
+    @Test
+    public void shouldCheckRamLimitOfCreatorUserInsteadOfCurrent() throws Exception {
+        final UserManager userManager = mock(UserManager.class);
+        final WorkspaceImpl ws = createRuntime("1gb", "1gb");
+        final User user = new User();
+        user.setName(ws.getNamespace());
+        doReturn(user).when(userManager).getByName(eq(ws.getNamespace()));
+
+        final LimitsCheckingWorkspaceManager manager = spy(new LimitsCheckingWorkspaceManager(2,
+                                                                                              "2gb", // <- workspaces ram limit
+                                                                                              "1gb",
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              userManager,
+                                                                                              false,
+                                                                                              false));
+
+        doReturn(ws).when(manager).getWorkspace(anyString()); // <- currently running 2gb
+        doReturn(ws).when(manager).checkRamAndPropagateStart(anyObject(), anyString(), anyString(), anyObject());
+
+        manager.startWorkspace(ws.getId(), null, null);
+
+        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
+        verify(manager).checkRamAndPropagateStart(anyObject(), anyString(), argument.capture(), anyObject());
+        Assert.assertEquals(argument.getValue(), ws.getNamespace());
+    }
+
+    @Test(expectedExceptions = ServerException.class,
+          expectedExceptionsMessageRegExp = "Unable to start workspace .*, because its namespace owner is " +
+                                            "unavailable and it is impossible to check resources consumption.")
+    public void shouldPreventStartIfCreatorNotExistsAnymore() throws Exception {
+        final UserManager userManager = mock(UserManager.class);
+        final WorkspaceImpl ws = createRuntime("1gb", "1gb");
+        doThrow(new NotFoundException("Nope")).when(userManager).getByName(eq(ws.getNamespace()));
+
+
+        final LimitsCheckingWorkspaceManager manager = spy(new LimitsCheckingWorkspaceManager(2,
+                                                                                              "2gb", // <- workspaces ram limit
+                                                                                              "1gb",
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              null,
+                                                                                              userManager,
+                                                                                              false,
+                                                                                              false));
+        doReturn(ws).when(manager).getWorkspace(anyString()); // <- currently running 2gb
+
+        manager.startWorkspace(ws.getId(), null, null);
     }
 }
