cefcb7b95a00c856d60fa530eb530d6852e8c005
Mykola Morhun <mmorhun@codenvy.com>
2016-07-14 12:26:21 +0300

    CODENVY-413: Make it possible to mark MachineNode as 'sheduled for maintenace' and not start new containers on it
    Signed-off-by: Mykola Morhun <mmorhun@codenvy.com>

---
+++ plugins/plugin-hosted/codenvy-machine-hosted/src/main/java/com/codenvy/machine/MaintenanceConstraintProvider.java
@@ -0,0 +1,39 @@
+/*
+ *  [2012] - [2016] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.machine;
+
+import com.google.inject.Provider;
+
+/**
+ * Provides constraint for images and containers for prevent actions with them on node under maintenance.
+ *
+ * @author Mykola Morhun
+ */
+public class MaintenanceConstraintProvider implements Provider<String> {
+    public static final String MAINTENANCE_CONSTRAINT_KEY = "constraint:com.codenvy.node-state!";
+    public static final String MAINTENANCE_CONSTRAINT_VALUE = "maintenance";
+    /**
+     * This constraint
+     * <i>constraint:com.codenvy.node-state!=maintenance</i>
+     * prevent build / start machine on a node with maintenance status
+     */
+    public static final String MAINTENANCE_CONSTRAINT = MAINTENANCE_CONSTRAINT_KEY + '=' + MAINTENANCE_CONSTRAINT_VALUE;
+
+    @Override
+    public String get() {
+        return MAINTENANCE_CONSTRAINT;
+    }
+
+}
