5b8cdb9b94606e820cad1bdaba721109b988a1d9
Aleksandr Andrienko <aandrienko@codenvy.com>
2016-10-21 17:59:58 +0300

    CHE-1518: Fix creation zombie prosess when user closes terminal. (#570)
    Fix creation zombie processes when user closes terminal by close button or prints command exit in the terminal.
    
    Signed-off-by: Aleksandr Andrienko <aandrienko@codenvy.com>

--- lib/websocket-terminal/src/main/go/server.go
+++ lib/websocket-terminal/src/main/go/server.go
@@ -33,11 +33,16 @@ import (
 	"unicode/utf8"
 	"regexp"
 	"strings"
+	"net"
+	"errors"
+	"io/ioutil"
+	"sync"
+	"fmt"
 )
 
 type wsPty struct {
-	Cmd *exec.Cmd // pty builds on os.exec
-	Pty *os.File  // a pty is simply an os.File
+	Cmd     *exec.Cmd // pty builds on os.exec
+	PtyFile *os.File  // a pty is simply an os.File
 }
 
 type route struct {
@@ -47,6 +52,17 @@ type route struct {
 
 type routes struct {
 	routes []*route
+}
+
+type WebSocketMessage struct {
+	Type string          `json:"type"`
+	Data json.RawMessage `json:"data"`
+}
+
+type ReadWriteRoutingFinalizer struct {
+	*sync.Mutex
+	readDone  bool
+	writeDone bool
 }
 
 var (
@@ -118,31 +134,165 @@ func (router *routes) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
-func (wp *wsPty) Start() {
-	var err error
-	args := flag.Args()
-	wp.Cmd = exec.Command(cmdFlag, args...)
-	env := os.Environ()
-	env = append(env, "TERM=xterm")
-	wp.Cmd.Env = env
-	wp.Pty, err = pty.Start(wp.Cmd)
+func StartPty() (*wsPty, error) {
+	// TODO consider whether these args are needed at all
+	cmd := exec.Command(cmdFlag, flag.Args()...)
+	cmd.Env = append(os.Environ(), "TERM=xterm")
+
+	file, err := pty.Start(cmd)
 	if err != nil {
-		log.Fatalf("Failed to start command: %s\n", err)
+		return nil, err
 	}
+
 	//Set the size of the pty
-	pty.Setsize(wp.Pty, 60, 200)
+	pty.Setsize(file, 60, 200)
+
+	return &wsPty{
+		PtyFile: file,
+		Cmd:     cmd,
+	}, nil
 }
 
-func (wp *wsPty) Stop() {
-	wp.Pty.Close()
-	wp.Cmd.Wait()
+func isNormalWsError(err error) bool {
+	closeErr, ok := err.(*websocket.CloseError)
+	if ok && (closeErr.Code == websocket.CloseGoingAway || closeErr.Code == websocket.CloseNormalClosure) {
+		return true
+	}
+	_, ok = err.(*net.OpError)
+	return ok
+}
+
+func isNormalPtyError(err error) bool {
+	if err == io.EOF {
+		return true
+	}
+	pathErr, ok := err.(*os.PathError)
+	return ok &&
+		pathErr.Op == "read" &&
+		pathErr.Path == "/dev/ptmx" &&
+		pathErr.Err.Error() == "input/output error"
+}
+
+// read from the web socket, copying to the pty master
+// messages are expected to be text and base64 encoded
+func sendConnectionInputToPty(conn *websocket.Conn, reader io.ReadCloser, f *os.File, finalizer *ReadWriteRoutingFinalizer) {
+	defer closeReader(reader, f, finalizer)
+
+	for {
+		mt, payload, err := conn.ReadMessage()
+		if err != nil {
+			if !isNormalWsError(err) {
+				log.Printf("conn.ReadMessage failed: %s\n", err)
+			}
+			return
+		}
+		switch mt {
+		case websocket.BinaryMessage:
+			log.Printf("Ignoring binary message: %q\n", payload)
+		case websocket.TextMessage:
+			var msg WebSocketMessage
+			if err := json.Unmarshal(payload, &msg); err != nil {
+				log.Printf("Invalid message %s\n", err)
+				continue
+			}
+			if errMsg := handleMessage(msg, f); errMsg != nil {
+				log.Printf(errMsg.Error())
+				return
+			}
+
+		default:
+			log.Printf("Invalid websocket message type %d\n", mt)
+			return
+		}
+	}
+}
+
+func handleMessage(msg WebSocketMessage, ptyFile *os.File) error {
+	switch msg.Type {
+	case "resize":
+		var size []float64
+		if err := json.Unmarshal(msg.Data, &size); err != nil {
+			log.Printf("Invalid resize message: %s\n", err)
+		} else {
+			pty.Setsize(ptyFile, uint16(size[1]), uint16(size[0]))
+			activity.Notify();
+		}
+
+	case "data":
+		var dat string
+		if err := json.Unmarshal(msg.Data, &dat); err != nil {
+			log.Printf("Invalid data message %s\n", err)
+		} else {
+			ptyFile.Write([]byte(dat));
+			activity.Notify();
+		}
+
+	default:
+		return errors.New("Invalid field message type: " + msg.Type + "\n")
+	}
+	return nil
+}
+
+//read byte array as Unicode code points (rune in go)
+func normalizeBuffer(normalizedBuf *bytes.Buffer, buf []byte, n int) (int, error) {
+	bufferBytes := normalizedBuf.Bytes()
+	runeReader := bufio.NewReader(bytes.NewReader(append(bufferBytes[:], buf[:n]...)))
+	normalizedBuf.Reset()
+	i := 0
+	for i < n {
+		char, charLen, err := runeReader.ReadRune()
+		if err != nil {
+			return i, err
+		}
+		if char == utf8.RuneError {
+			runeReader.UnreadRune()
+			return i, nil
+		}
+		i += charLen
+		if _, err := normalizedBuf.WriteRune(char); err != nil {
+			return i, err
+		}
+	}
+	return i, nil
+}
+
+// copy everything from the pty master to the websocket
+// using base64 encoding for now due to limitations in term.js
+func sendPtyOutputToConnection(conn *websocket.Conn, reader io.ReadCloser, finalizer *ReadWriteRoutingFinalizer) {
+	defer closeConn(conn, finalizer)
+
+	buf := make([]byte, 8192)
+	var buffer bytes.Buffer
+	// TODO: more graceful exit on socket close / process exit
+	for {
+		n, err := reader.Read(buf)
+		if err != nil {
+			if !isNormalPtyError(err) {
+				log.Printf("Failed to read from pty: %s", err)
+			}
+			return
+		}
+		i, err := normalizeBuffer(&buffer, buf, n)
+		if err != nil {
+			log.Printf("Cound't normalize byte buffer to UTF-8 sequence, due to an error: %s", err.Error())
+			return
+		}
+		if err = conn.WriteMessage(websocket.TextMessage, buffer.Bytes()); err != nil {
+			log.Printf("Failed to send websocket message: %s, due to occurred error %s", string(buffer.Bytes()), err.Error())
+			return
+		}
+		buffer.Reset()
+		if i < n {
+			buffer.Write(buf[i:n])
+		}
+	}
 }
 
 func ptyHandler(w http.ResponseWriter, r *http.Request) {
 	tokenParam := r.URL.Query().Get("token")
 	if tokenParam == "" {
 		w.WriteHeader(http.StatusUnauthorized)
-		log.Printf("Authentication failed: missing token.\n")
+		log.Println("Authentication failed: missing token.")
 		return
 	} else {
 		req, err := http.NewRequest("GET", apiEndpoint + "/machine/token/user/" + tokenParam, nil)
@@ -169,113 +319,63 @@ func ptyHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatalf("Websocket upgrade failed: %s\n", err)
 	}
-	defer conn.Close()
 
-	wp := wsPty{}
-	// TODO: check for errors, return 500 on fail
-	wp.Start()
-
-	// copy everything from the pty master to the websocket
-	// using base64 encoding for now due to limitations in term.js
-	go func() {
-		buf := make([]byte, 8192)
-		reader := bufio.NewReader(wp.Pty)
-		var buffer bytes.Buffer;
-		// TODO: more graceful exit on socket close / process exit
-		for {
-			n, err := reader.Read(buf);
-
-			if err != nil {
-				log.Printf("Failed to read from pty master: %s", err)
-				return
-			}
-			//read byte array as Unicode code points (rune in go)
-
-			bufferBytes := buffer.Bytes()
-			runeReader := bufio.NewReader(bytes.NewReader(append(bufferBytes[:], buf[:n]...)))
-			buffer.Reset()
-			i := 0;
-			for i < n {
-				char, charLen, e := runeReader.ReadRune()
-				if e != nil {
-					log.Printf("Failed to read from pty master: %s", err)
-					return
-				}
-
-				if char == utf8.RuneError {
-					runeReader.UnreadRune()
-					break
-				}
-				i += charLen;
-				buffer.WriteRune(char)
-			}
-			err = conn.WriteMessage(websocket.TextMessage, buffer.Bytes())
-			if err != nil {
-				log.Printf("Failed to send UTF8 char: %s", err)
-				return
-			}
-			buffer.Reset();
-			if i < n {
-				buffer.Write(buf[i:n])
-			}
-		}
-	}()
-
-	type Message struct {
-		Type string `json:"type"`
-		Data json.RawMessage `json:"data"`
+	wp, err := StartPty()
+	if err != nil {
+		log.Fatalf("Failed to start command: %s\n", err)
+		return
 	}
 
-	// read from the web socket, copying to the pty master
-	// messages are expected to be text and base64 encoded
-	for {
-		mt, payload, err := conn.ReadMessage()
-		if err != nil {
-			if err != io.EOF {
-				log.Printf("conn.ReadMessage failed: %s\n", err)
-				return
-			}
-		}
-		var msg Message;
-		switch mt {
-		case websocket.BinaryMessage:
-			log.Printf("Ignoring binary message: %q\n", payload)
-		case websocket.TextMessage:
-			err := json.Unmarshal(payload, &msg);
-			if err != nil {
-				log.Printf("Invalid message %s\n", err);
-				continue
-			}
-			switch msg.Type{
-			case "resize" :
-				var size []float64;
-				err := json.Unmarshal(msg.Data, &size)
-				if err != nil {
-					log.Printf("Invalid resize message: %s\n", err);
-				} else {
-					pty.Setsize(wp.Pty, uint16(size[1]), uint16(size[0]));
-					activity.Notify();
-				}
+	reader := ioutil.NopCloser(wp.PtyFile)
+	finalizer := ReadWriteRoutingFinalizer{&sync.Mutex{}, false, false}
 
-			case "data" :
-				var dat string;
-				err := json.Unmarshal(msg.Data, &dat);
-				if err != nil {
-					log.Printf("Invalid data message %s\n", err);
-				} else {
-					wp.Pty.Write([]byte(dat));
-					activity.Notify();
-				}
+	defer waitAndClose(wp, &finalizer, conn, reader)
 
-			default:
-				log.Printf("Invalid message type %d\n", mt)
-				return
-			}
+	//read output from terminal
+	go sendPtyOutputToConnection(conn, reader, &finalizer)
+	//write input to terminal
+	go sendConnectionInputToPty(conn, reader, wp.PtyFile, &finalizer)
 
-		default:
-			log.Printf("Invalid message type %d\n", mt)
-			return
-		}
+	fmt.Println("New terminal succesfully initialized.")
+}
+
+func waitAndClose(wp *wsPty, finalizer *ReadWriteRoutingFinalizer, conn *websocket.Conn, reader io.ReadCloser) {
+	if err := wp.Cmd.Wait(); err != nil {
+		log.Printf("Failed to stop process, due to occurred error '%s'", err.Error())
 	}
-	wp.Stop()
+
+	wp.PtyFile.Close();
+
+	closeConn(conn, finalizer)
+	closeReader(reader, wp.PtyFile, finalizer)
+
+	fmt.Println("Terminal process completed.")
+}
+
+func closeReader(reader io.ReadCloser, file *os.File, finalizer *ReadWriteRoutingFinalizer) {
+	defer finalizer.Unlock()
+
+	finalizer.Lock()
+	if !finalizer.readDone {
+		closeReaderErr := reader.Close()
+		if (closeReaderErr != nil) {
+			log.Printf("Failed to close pty file reader '%s'" + closeReaderErr.Error())
+		}
+		//hack to prevent suspend reader on the operation read when file has been already closed.
+		file.Write([]byte("0"));
+		finalizer.readDone = true;
+		fmt.Println("Terminal reader closed.")
+	}
+
+}
+
+func closeConn(conn *websocket.Conn, finalizer *ReadWriteRoutingFinalizer) {
+	defer finalizer.Unlock()
+
+	finalizer.Lock()
+	if !finalizer.writeDone {
+		conn.Close()
+		finalizer.writeDone = true
+		fmt.Println("Terminal writer closed.")
+	}
 }
