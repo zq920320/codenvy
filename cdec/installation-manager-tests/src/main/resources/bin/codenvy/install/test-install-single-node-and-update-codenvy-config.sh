418f0bef3d952fe4b28863e2e2e72726517e96bc
Dmytro Nochevnov <dnochevnov@codenvy.com>
2016-11-08 17:56:55 +0200

    Replace api.enpoint on che.api in IM integration tests (#1117)
    Signed-off-by: Dmytro Nochevnov <dnochevnov@codenvy.com>

--- cdec/installation-manager-tests/src/main/resources/bin/codenvy/install/test-install-single-node-and-update-codenvy-config.sh
+++ cdec/installation-manager-tests/src/main/resources/bin/codenvy/install/test-install-single-node-and-update-codenvy-config.sh
@@ -61,11 +61,11 @@ executeIMCommand "config" "--hostname" "${NEW_HOST_URL}"
 
 # verify changes on api node
 executeSshCommand "sudo cat /home/codenvy/codenvy-data/conf/general.properties"
-executeSshCommand "sudo grep \"api.endpoint=http://${NEW_HOST_URL}/api\" /home/codenvy/codenvy-data/conf/general.properties"
+executeSshCommand "sudo grep \"che.api=http://${NEW_HOST_URL}/api\" /home/codenvy/codenvy-data/conf/general.properties"
 
 # verify changes on installation-manager service
 executeSshCommand "sudo cat /home/codenvy-im/codenvy-im-data/conf/installation-manager.properties"
-executeSshCommand "sudo grep \"api.endpoint=http://${NEW_HOST_URL}/api\" /home/codenvy-im/codenvy-im-data/conf/installation-manager.properties"
+executeSshCommand "sudo grep \"che.api=http://${NEW_HOST_URL}/api\" /home/codenvy-im/codenvy-im-data/conf/installation-manager.properties"
 
 authWithoutRealmAndServerDns "admin" "password" "http://${NEW_HOST_URL}"
 
