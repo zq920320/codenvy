6809e94c03f41ba40eb273e7b54f6d23706f4ab1
Florent BENOIT <fbenoit@codenvy.com>
2016-11-21 15:47:41 +0100

    Cleanup copyright headers move entrypoints to be named "entrypoint.sh" fix indent
    Change-Id: I0cc387daff6296aae3e03ef8dab3c6266bfab36e
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dockerfiles/haproxy/entrypoint.sh
+++ dockerfiles/haproxy/entrypoint.sh
@@ -1,15 +1,19 @@
+#!/bin/sh
 # Copyright (c) 2016 Codenvy, S.A.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 #
-# build:
-#   docker build -t codenvy/puppet:<version> .
-#
-# use:
-#    docker run codenvy/puppet:<version>
 
-FROM puppet/puppet-agent-alpine:4.6.2
-RUN apk --update add openssh
+# Make sure service is running
+exec rsyslogd -n &
 
+# Touch the log file so we can tail on it
+touch /var/log/haproxy/haproxy.log
+
+# Throw the log to output
+tail -f /var/log/haproxy/haproxy.log &
+
+# Start haproxy
+exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
--- dockerfiles/init/entrypoint.sh
+++ dockerfiles/init/entrypoint.sh
@@ -1,15 +1,16 @@
+#!/bin/sh
 # Copyright (c) 2016 Codenvy, S.A.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 #
-# build:
-#   docker build -t codenvy/puppet:<version> .
-#
-# use:
-#    docker run codenvy/puppet:<version>
 
-FROM puppet/puppet-agent-alpine:4.6.2
-RUN apk --update add openssh
-
+# Make sure service is running
+cp -rf /files/manifests /copy
+cp -rf /files/modules /copy
+cp -rf /files/docs /copy
+# do not copy codenvy.env if exist
+if [ ! -f  /copy/codenvy.env ]; then
+    cp /files/manifests/codenvy.env /copy
+fi
