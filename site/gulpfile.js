f4aac25dd428aac46b1b2d42edd202c3ff2e7268
Viktor Kuznyetsov <vkuznyetsov@codenvy.com>
2016-11-25 14:40:26 +0200

    CODENVY-1051 apply new styles in site pages (#1216)
    * CODENVY-1051 apply new styles in site pages
    
    * CODENVY-1051 make error message in red
    
    * CODENVY-1051 download font locally

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -26,20 +26,13 @@ var gulp = require('gulp'),
 
 var buildConfig = {
         jekyllStageConfig : "_config.stage.yml", //saas-stage
-        jekyllProdConfig : "_config.prod.yml", //saas-prod
-        jekyllGHConfig : "_config.gh.yml", //github-pages
-        jekyllEEConfig : "_config.enterprise.yml", //onprem-ccis
         jekyllOnpremSEConfig : "_config.onprem-se.yml" //onprem-se
     };
 
 var paths = {
         src: 'app/',
          // assembly folders
-        prod: './target/prod/',
-        stage: './target/stage/',
-        enterprise: './target/enterprise/',
         onpremSE: './target/onprem-se/',
-        gh: './target/gh/',
         // ------------------
         temp: 'target/temp/',
         dist: 'target/dist/', //dist folder
@@ -52,143 +45,6 @@ gulp.task('connect', ['gh'], function() {
     root: paths.gh
   });
 });
-
-// --------------------------- Building Prod -----------------------------
-//----------------
-//----------
-gulp.task('prod',['copy_src','duplicate_html','lint','prod_cfg','css','jekyll','rjs','myrev','replace','rmbuild','copy_prod'], function(){
-
-})
-
-gulp.task('prod_cfg', function(){
-  return gulp.src(paths.config + buildConfig.jekyllProdConfig)
-  .pipe(rename('_config.yml'))
-  .pipe(gulp.dest(paths.temp))
-})
-
-gulp.task('css', ['copy_src','jekyll'], function() {
-  return gulp.src(paths.temp+'site/styles/*.css')
-  .pipe(minifyCSS())
-  .pipe(gulp.dest(paths.prod + 'site/styles/'));
-});
-
-gulp.task('lint',function(){
-  gulp.src(['!'+paths.src+'site/scripts/vendor/*.*',paths.src+'site/scripts/**/*.js'])
-    .pipe(jshint())
-  .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(jshint.reporter('fail'))
-});
-
-// Builds projects using require.js's optimizer + Minify files with UglifyJS
-gulp.task('rjs',['copy_src','jekyll'], function(){
-      return  rjs({
-            mainConfigFile: paths.temp +'site/scripts/main.js',
-            //optimize: 'none', //hardcoded in requirejs plugin
-            baseUrl: paths.temp + 'site/scripts',
-            wrap: true,
-            name: 'main',
-            mainFile: paths.temp+'site/index.html',
-            out: 'amd-main.js'
-        })
-      .pipe(print(function(filepath) {
-        return "rjs -> built: " + filepath;
-      }))
-      .pipe(gulp.src(paths.temp +'site/scripts/vendor/require.js'))
-      .pipe(reverse({objectMode: true})) // requirejs should be at the begining
-      .pipe(concat('amd-app.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(paths.prod + 'site/scripts'));
- });
-
-gulp.task('jekyll',['copy_src','prod_cfg'], function () {
-       console.log('Jekyll building ......... ');
-   return gulp.src(paths.temp+'_config.yml', {read: false})
-    .pipe(shell([
-      'jekyll build'
-    ], {
-      cwd: 'target/temp',
-      templateData: {
-        f: function (s) {
-          return s.replace(/$/, '.bak')
-        }
-      }
-    }))
-});
-
-// Replaces references with reved names
-  gulp.task ('myrev', ['copy_src','prod_cfg','css','rjs','jekyll'], function(){
-    return gulp.src([paths.prod + 'site/scripts/amd-app.js', paths.prod + 'site/styles/*.css'],{base:paths.prod})
-    //.pipe(wait(1500))
-    .pipe(rev())
-    .pipe(print(function(filepath) {
-        return "rev -> built: " + filepath;
-      }))
-    .pipe(gulp.dest(paths.prod))
-    .pipe(rev.manifest())
-    .pipe(print(function(filepath) {
-        return "manifest -> built: " + filepath;
-      }))
-    .pipe(gulp.dest(paths.prod));
-  });
-
-// Start replacing rev references
-gulp.task('replace',['copy_src','prod_cfg','css','rjs','jekyll','myrev'], function(){
-
-  var manifest = require(paths.prod+'rev-manifest.json')
-  console.log('Process rev-manifest.json: \n');
-  console.log(manifest);
-  return gulp.src([paths.prod+'**/*.html'])
-  .pipe(replaceRevRef({
-    base: process.cwd()+'/target/prod',
-    manifest: manifest,
-    path: ''/*,
-    cdnPrefix: 'http://absolute.path/cdn/'*/
-  }))
-  .pipe(gulp.dest(paths.prod));
-
-});
-
-// Removes <!-- build:js ... blocks from html for prod configeration
-gulp.task('rmbuild', ['copy_src','prod_cfg','css','rjs','jekyll','myrev','replace'], function(){
-  return gulp.src(paths.prod+'**/*.html')
-  .pipe(useref())
-  .pipe(print(function(filepath) {
-        return "remove <!-- build:js ... blocks -> built: " + filepath;
-      }))
-  .pipe(gulp.dest(paths.prod));
-
-});
-
-gulp.task('copy_prod',['copy_src','duplicate_html','prod_cfg','css','rjs','jekyll','myrev','replace','rmbuild'], function(){
-  gulp.src([paths.prod+'/**/*.html', // all HTML
-    '!'+paths.prod+'site/custom_pages/**/*.html',
-    '!'+paths.prod+'site/admin.html',
-    '!'+paths.prod+'site/email-templates_onpremises/*.html',
-    paths.prod+'**/amd-app-*.js', // minified JS
-    paths.prod+'**/*-*.css', // minified CSS
-    paths.prod+'**/*.jpg',
-    paths.prod+'**/*.ico',
-    paths.prod+'**/*.png',
-    paths.prod+'**/*.svg',
-    paths.prod+'**/*.txt',  // robots.txt
-    paths.prod+'**/modernizr.custom.*.js']
-    )
-      .pipe(print(function(filepath) {
-        if (filepath){return "copy prod -> built: " + filepath;}
-        
-      }))
-  .pipe(gulp.dest(paths.dist+'prod'));
-});
-
-// Cleans gulp's folders
-gulp.task('clean',function(){
-  return gulp.src([paths.temp,paths.prod,paths.stage,paths.dist,paths.enterprise],{ read: false }) // much faster
-    .pipe(print(function(filepath) {
-      return "Delete: " + filepath;
-    }))
-    .pipe(rimraf());
-})
-
 // --------------------------- Building Stage -----------------------------
 //----------------
 //----------
@@ -258,129 +114,15 @@ gulp.task('copy_stage',['copy_src','stage_cfg','css_stage','jekyll_stage'], func
     paths.prod+'**/*.ico',
     paths.stage+'**/*.png',
     paths.stage+'**/*.svg',
+    paths.stage+'**/*.woff',
+    paths.stage+'**/*.woff2',
+    paths.stage+'**/*.ttf',
+    paths.stage+'**/*.eot',
+    paths.stage+'**/*.otf',
     paths.stage+'**/*.txt'  // robots.txt
     ])
   .pipe(gulp.dest(paths.dist+'stage'));
 });
-
-
-// --------------------------- Building On-premises CCCIS (LDAP version) ----------------------------- 
-//----------------
-//----------
-gulp.task('enterprise',
-  ['copy_src',
-  'enterprise_cfg',
-  'css_enterprise',
-  'jekyll_enterprise',
-  'copy_enterprise',
-  'enterprise_login_page',
-  'copy_email_templates',
-  'del_replaced_files_ldap',
-  'enterprise_copy_templates'],
-  function(){
-})
-
-gulp.task('enterprise_cfg', function(){
-  console.log(buildConfig.jekyllEEConfig);
-  return gulp.src(paths.config + buildConfig.jekyllEEConfig)
-  .pipe(rename('_config.yml'))
-  .pipe(gulp.dest(paths.temp))
-})
-
-// Copy enterprise login custom page
-gulp.task('enterprise_login_page', ['copy_src','enterprise_cfg','css_enterprise','del_replaced_files_ldap','jekyll_enterprise'], function(){
-    console.log(paths.enterprise + 'site/custom_pages/cccis/login.html');
-  return   gulp.src(paths.enterprise + 'site/custom_pages/cccis/login.html')
-  .pipe(gulp.dest(paths.enterprise+'site'))
-  .pipe(rename('index.html'))
-  .pipe(gulp.dest(paths.enterprise))
-  .pipe(print(function(filepath) {
-    return "Copy enterprise login pages to ->" + filepath;
-  }));
-});
-
-// Cleaning files and folders to be replaced with custom ones
-gulp.task('del_replaced_files_ldap',['copy_src','enterprise_cfg','css_enterprise','jekyll_enterprise'], function(){
-  return del([
-    paths.enterprise+'site/login.html',
-    paths.enterprise+'index.html'
-    ]);
-});
-
-// Copy enterprise custom pages
-gulp.task('enterprise_copy_templates', ['copy_src','enterprise_cfg','css_enterprise','clean_templates_ldap','del_replaced_files_ldap','jekyll_enterprise'], function(){
-  return   gulp.src(paths.enterprise + 'site/custom_pages/cccis/templates/*.html')
-  .pipe(gulp.dest(paths.enterprise+'site/templates'))
-  .pipe(print(function(filepath) {
-    return "Copy enterprise custom pages to ->" + filepath;
-  }));
-});
-
-gulp.task('css_enterprise', ['copy_src'], function() {
-  return gulp.src(paths.temp+'site/styles/*.scss')
-  .pipe(compass({
-    //config_file: './compass-config.rb',
-    css: paths.temp +'site/styles',
-    sass: paths.temp +'site/styles'
-  }))
-  .pipe(gulp.dest(paths.enterprise + 'site/styles/'));
-});
-// Ensure waiting for Jekill job finishing
-gulp.task('jekyll_enterprise',['copy_src','enterprise_cfg'], function () {
-         console.log('Jekyll enterprise......... ');
-   return gulp.src(paths.temp+'_config.yml', {read: false})
-    .pipe(shell([
-      'jekyll build'
-    ], {
-      cwd: 'target/temp',
-      templateData: {
-        f: function (s) {
-          return s.replace(/$/, '.bak')
-        }
-      }
-    }))
-});
-
-gulp.task('copy_enterprise',
-  ['copy_src',
-  'enterprise_cfg',
-  'css_enterprise',
-  'jekyll_enterprise',
-  'clean_templates_ldap',
-  'del_replaced_files_ldap',
-  'enterprise_login_page',
-  'enterprise_copy_templates'], function(){
-  gulp.src([paths.enterprise+'**/*.html', // all HTML
-    '!'+paths.enterprise+'site/custom_pages/**/*.html',
-    '!'+paths.enterprise+'site/email-templates_onpremises/*.html',
-    '!'+paths.enterprise+'site/email-templates/*.html',
-    '!'+paths.enterprise+'site/recover-password.html',
-    '!'+paths.enterprise+'site/setup-password.html',
-    '!'+paths.enterprise+'index.html',
-    paths.enterprise+'**/*.js',
-    paths.enterprise+'**/*.css',
-    paths.enterprise+'**/*.jpg',
-    paths.prod+'**/*.ico',
-    paths.enterprise+'**/*.png',
-    paths.enterprise+'**/*.svg',
-    paths.enterprise+'**/*.txt'  // robots.txt
-    ])
-  .pipe(gulp.dest(paths.dist+'enterprise'));
-});
-
-// Copy omprem email templates page
-gulp.task('copy_email_templates', ['copy_src','enterprise_cfg','css_enterprise','jekyll_enterprise','copy_enterprise'], function(){
-  return   gulp.src(paths.enterprise+'site/email-templates_onpremises/*.html')
-    .pipe(gulp.dest(paths.dist+'enterprise/site/email-templates'))
-});
-
-// clean prod templates
-gulp.task('clean_templates_ldap',['copy_src','enterprise_cfg','css_enterprise','jekyll_enterprise'], function(){
-  return gulp.src(paths.enterprise + '/site/templates/')
-  .pipe(rimraf());
-});
-
-//***************************************************************** the end On-premises (LDAP version)
 
 // --------------------------- Building On-premises SE (standart edition version) ----------------------------- path.onpremSE
 //----------------
@@ -557,6 +299,11 @@ gulp.task('copy_onprem_se',
     paths.onpremSE+'**/*.ico',
     paths.onpremSE+'**/*.png',
     paths.onpremSE+'**/*.svg',
+    paths.onpremSE+'**/*.woff',
+    paths.onpremSE+'**/*.woff2',
+    paths.onpremSE+'**/*.ttf',
+    paths.onpremSE+'**/*.eot',
+    paths.onpremSE+'**/*.otf',
     paths.onpremSE+'**/modernizr.custom.*.js',
     paths.onpremSE+'**/*.txt'  // robots.txt
     ])
@@ -568,57 +315,7 @@ gulp.task('copy_onprem_se_email_templates', ['copy_src','onprem_se_cfg','css_onp
   return   gulp.src(paths.onpremSE+'site/email-templates_onpremises/*.html')
     .pipe(gulp.dest(paths.dist+'onprem-se/site/email-templates'))
 });
-
 //***************************************************************** On-premises (base version)
-
-// --------------------------- Building GitHub pages (localhost:8080) -----------------------------
-//----------------
-//----------
-gulp.task('gh',['copy_src','gh_cfg','css_gh','jekyll_gh','copy_gh'], function(){
-  console.log('Update localhost .....');
-})
-
-gulp.task('gh_cfg', function(){
-  return gulp.src(paths.config + buildConfig.jekyllGHConfig)
-  .pipe(rename('_config.yml'))
-  .pipe(gulp.dest(paths.temp))
-})
-
-gulp.task('css_gh', ['copy_src'], function() {
-  return gulp.src(paths.temp+'site/styles/*.scss')
-  .pipe(compass({
-    //config_file: './compass-config.rb',
-    css: paths.temp +'site/styles',
-    sass: paths.temp +'site/styles'
-  }))
-  .pipe(gulp.dest(paths.gh + 'site/styles/'));
-});
-
-gulp.task('jekyll_gh',['copy_src','gh_cfg'], function () {
-         console.log('Jekyll ......... ');
-     return require('child_process')
-        .spawn('jekyll', ['build'], {stdio: 'inherit', cwd: paths.temp});
-
-});
-
-gulp.task('copy_gh',['copy_src','gh_cfg','css_gh','jekyll_gh'], function(){
-  gulp.src([paths.gh+'/**/*.html', // all HTML
-    paths.gh+'**/*.js',
-    paths.gh+'**/*.css',
-    paths.gh+'**/*.jpg',
-    paths.gh+'**/*.png',
-    paths.gh+'**/*.svg',
-    paths.gh+'**/*.txt'  // robots.txt
-    ])
-  .pipe(gulp.dest(paths.dist+'gh'));
-});
-
-gulp.task('watch', function(){
-  console.log('Watching for changes in : '+paths.src+'*.*');
-return gulp.watch(''+paths.src+'**/*.*',['gh']);
-
-
-});
 
 // --------------------------- Dev config for LiveReload localhost:8080) -----------------------------
 //----------------
