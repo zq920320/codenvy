df0d6ec02590597610eb297ae2dd98115bda2db8
Roman Iuvshin <riuvshin@codenvy.com>
2016-11-25 17:21:28 +0200

    gracefully stop ws master with SIGTERM, fix typos, add stop timeout 3min (#1228)
    * gracefully stop ws master with SIGTERM, fix typos, add stop timeout 3min for stop

--- dockerfiles/codenvy/entrypoint.sh
+++ dockerfiles/codenvy/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright (c) 2016 Codenvy, S.A.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
@@ -6,11 +6,6 @@
 # http://www.eclipse.org/legal/epl-v10.html
 #
 
-#
-# Start postfix
-#
-postfix -c /etc/postfix start
-#
 #
 # Check if Postgres is running. There's a 5 min timeout
 # for Postgres to start. If it does not Codenvy Tomcat
@@ -28,20 +23,22 @@ fi
 
 echo "Waiting for Postgres to boot..."
 
-MAX_TRIES=60 
+MAX_TRIES=60
 COUNT=0
-    while [ $COUNT -lt $MAX_TRIES ]; do
+  while [ $COUNT -lt $MAX_TRIES ]; do
     psql -h postgres -d template1 -c '\l'
 	if [ $? -eq 0 ]; then
     	echo '[INFO]: Postgres is up and running. Starting Codenvy Tomcat...'
     	break
 	fi
-	    let COUNT=COUNT+1
-	    echo '[INFO]: Postgres is unavailable. Trying in 5 seconds...'
-	    sleep 5
-               if [ "$COUNT" -eq "60" ]; then
-                  echo "[ERROR]: Postgres failed to start in 5 minutes. Check Postgres container logs. Codenvy Tomcat start canceled."
-                  exit
-               fi
-         done
-exec "$@"
+      let COUNT=COUNT+1
+      echo '[INFO]: Postgres is unavailable. Trying in 5 seconds...'
+      sleep 5
+             if [ "$COUNT" -eq "60" ]; then
+                echo "[ERROR]: Postgres failed to start in 5 minutes. Check Postgres container logs. Codenvy Tomcat start canceled."
+                exit
+             fi
+  done
+
+# start codenvy tomcat
+exec /opt/codenvy-tomcat/bin/catalina.sh ${JPDA} run
