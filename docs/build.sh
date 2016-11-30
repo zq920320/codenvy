616bdb9e051a67427438222e42ac0cb9298c2d41
Tyler Jewell <tjewell@codenvy.com>
2016-11-30 14:15:56 -0800

    docs

---
+++ docs/build.sh
@@ -0,0 +1,10 @@
+
+# Hardcoded for windows right now.
+# Need to make this configurable for our CI systems and to work on windows or linux.
+# 
+# Optionally - you can run 'jekyll/jekyll jekyll serve' to get a local server on port 9080
+# NOTE - these files will not work without a hosted server right now - they are not static stand alone 
+MSYS_NO_PATHCONV=1 docker run --rm -it -p 9080:4000 --name jekyll \
+       -v /C/codenvy/codenvy/docs:/srv/jekyll \
+       -v /C/codenvy/codenvy/docs/_site:/srv/jekyll/_site \
+           jekyll/jekyll jekyll build --watch
