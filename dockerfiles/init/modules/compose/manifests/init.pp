437b8ac371656399b5501dbf1ff48a2ccc349ca9
Roman Iuvshin <riuvshin@codenvy.com>
2016-12-06 12:55:19 +0200

    single template for compose files (#1289)

--- dockerfiles/init/modules/compose/manifests/init.pp
+++ dockerfiles/init/modules/compose/manifests/init.pp
@@ -1,13 +1,17 @@
 class compose {
-  file { "/opt/codenvy/docker-compose-container.yml":
-    ensure  => "present",
-    content => template("compose/docker-compose-container.yml.erb"),
-    mode    => '644',
+  define generate_compose_file($compose_file_name = $name, $compose_file_for_containers = false) {
+    file { "/opt/codenvy/$compose_file_name":
+      ensure  => "present",
+      content => template("compose/docker-compose.yml.erb"),
+      mode    => '644',
+    }
   }
 
-  file { "/opt/codenvy/docker-compose.yml":
-    ensure  => "present",
-    content => template("compose/docker-compose.yml.erb"),
-    mode    => '644',
+  compose::generate_compose_file { "docker-compose-container.yml" :
+    compose_file_for_containers => true
+  }
+
+  compose::generate_compose_file { "docker-compose.yml" :
+    compose_file_for_containers => false
   }
 }
