a8077260043979d9a226dedc09f0ce4f40f4ac7c
Tyler Jewell <tjewell@codenvy.com>
2016-12-07 11:11:35 -0800

    proxy

--- dockerfiles/cli/scripts/cli.sh
+++ dockerfiles/cli/scripts/cli.sh
@@ -90,9 +90,9 @@ cmd_init_reinit_pre_action() {
   sed -i'.bak' "s|#CODENVY_SWARM_NODES=.*|CODENVY_SWARM_NODES=${CODENVY_HOST}:23750|" "${REFERENCE_CONTAINER_ENVIRONMENT_FILE}"
 
   # For testing purposes only
-  HTTP_PROXY=8.8.8.8
-  HTTPS_PROXY=http://4.4.4.4:9090
-  NO_PROXY="locahost, *.local, swarm-mode"
+  #HTTP_PROXY=8.8.8.8
+  #HTTPS_PROXY=http://4.4.4.4:9090
+  #NO_PROXY="locahost, *.local, swarm-mode"
 
   if [[ ! ${HTTP_PROXY} = "" ]]; then
     sed -i'.bak' "s|#${CHE_PRODUCT_NAME}_HTTP_PROXY_FOR_${CHE_PRODUCT_NAME}=.*|${CHE_PRODUCT_NAME}_HTTP_PROXY_FOR_${CHE_PRODUCT_NAME}=${HTTP_PROXY}|" "${REFERENCE_CONTAINER_ENVIRONMENT_FILE}"
