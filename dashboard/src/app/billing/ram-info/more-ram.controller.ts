a0ad01edd1397a42f930cdf9ccce2a4ba04795da
Ann Shumilova <ashumilova@codenvy.com>
2017-01-03 10:32:33 +0200

    customer-saas-67: show all RAM info and ability to get more RAM
    Signed-off-by: Ann Shumilova <ashumilova@codenvy.com>

---
+++ dashboard/src/app/billing/ram-info/more-ram.controller.ts
@@ -0,0 +1,250 @@
+/*
+ *  [2015] - [2016] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+'use strict';
+import {CodenvySubscription} from '../../../components/api/codenvy-subscription.factory';
+import {CodenvyResourceLimits} from '../../../components/api/codenvy-resource-limits';
+
+export class MoreRamController {
+  /**
+   * Service for displaying dialogs.
+   */
+  private $mdDialog: angular.material.IDialogService;
+  /**
+   * Subscription API service.
+   */
+  private codenvySubscription: CodenvySubscription;
+  /**
+   * Lodash library.
+   */
+  private lodash: any;
+  /**
+   * Notification service.
+   */
+  private cheNotification: any;
+
+  /**
+   * New RAM value.
+   */
+  value: number;
+  /**
+   * Current account's id (set from outside).
+   */
+  accountId: string;
+  /**
+   * Callback controller (set from outside).
+   */
+  callbackController: any;
+  /**
+   * Provided free RAM. Is retrieved from license details.
+   */
+  freeRAM: number;
+  /**
+   * Price of the resources. Is retrieved from package details.
+   */
+  price: number;
+  /**
+   * Amount of resources, that are paid for. Is retrieved from package details.
+   */
+  amount: string;
+  /**
+   * Minimum amount of resources, that can be bought. Is retrieved from package details.
+   */
+  minValue: number;
+  /**
+   * Maximum amount of resources, that can be bought. Is retrieved from package details.
+   */
+  maxValue: number;
+  /**
+   * Package with RAM type.
+   */
+  ramPackage: any;
+  /**
+   * Loading state of the dialog.
+   */
+  isLoading: boolean;
+
+  /**
+   * @ngInject for Dependency injection
+   */
+  constructor ($mdDialog: angular.material.IDialogService, codenvySubscription: CodenvySubscription, lodash: any, cheNotification: any) {
+    this.$mdDialog = $mdDialog;
+    this.codenvySubscription = codenvySubscription;
+    this.lodash = lodash;
+    this.cheNotification = cheNotification;
+    this.isLoading = true;
+
+    this.getLicense();
+    this.getPackages();
+  }
+
+  /**
+   * Fetches the license details.
+   */
+  getLicense(): void {
+    this.codenvySubscription.fetchLicense(this.accountId).then(() => {
+      this.processLicense(this.codenvySubscription.getLicense(this.accountId));
+    }, (error: any) => {
+      if (error.status === 304) {
+        this.processLicense(this.codenvySubscription.getLicense(this.accountId));
+      }
+    });
+  }
+
+  /**
+   * Processes license, retrieves free resources info.
+   *
+   * @param license
+   */
+  processLicense(license: any): void {
+    let details = license.resourcesDetails;
+    let freeResources = this.lodash.find(details, (resource: any) => {
+      return resource.providerId === 'free';
+    });
+
+    if (!freeResources) {
+      this.freeRAM = 0;
+      return;
+    }
+
+    let ram = this.lodash.find(freeResources.resources, (resource: any) => {
+      return resource.type === CodenvyResourceLimits.RAM;
+    });
+    this.freeRAM = ram ? (ram.amount / 1024) : 0;
+  }
+
+  /**
+   * Fetches the list of packages.
+   */
+  getPackages(): void {
+    this.isLoading = true;
+    this.codenvySubscription.fetchPackages().then(() => {
+      this.isLoading = false;
+      this.processPackages(this.codenvySubscription.getPackages());
+    }, (error: any) => {
+      this.isLoading = false;
+      if (error.status === 304) {
+        this.processPackages(this.codenvySubscription.getPackages());
+      }
+    });
+  }
+
+  /**
+   * Processes packages to get RAM resources details.
+   *
+   * @param packages list of packages
+   */
+  processPackages(packages: Array<any>): void {
+    this.ramPackage = this.lodash.find(packages, (pack: any) => {
+      return pack.type === CodenvyResourceLimits.RAM;
+    });
+
+    if (!this.ramPackage) {
+      return;
+    }
+
+    let ramResource = this.lodash.find(this.ramPackage.resources, (resource: any) => {
+      return resource.type === CodenvyResourceLimits.RAM;
+    })
+
+    if (!ramResource) {
+      return;
+    }
+
+    this.price = ramResource.monthlyPrice;
+    this.amount = ramResource.amount + ' ' + ramResource.unit;
+    this.minValue = ramResource.minAmount / 1000; //TODO
+    this.maxValue = ramResource.maxAmount / 1000; //TODO
+    this.value = angular.copy(this.minValue);
+  }
+
+  /**
+   * Hides the dialog.
+   */
+  hide() {
+    this.$mdDialog.hide();
+  }
+
+  /**
+   * Requests more RAM based on subscription state.
+   */
+  getMoreRAM(): void {
+    this.isLoading = true;
+    this.codenvySubscription.fetchActiveSubscription(this.accountId).then(() => {
+      this.processSubscription(this.codenvySubscription.getActiveSubscription(this.accountId));
+    }, (error:  any) => {
+      this.processSubscription(this.codenvySubscription.getActiveSubscription(this.accountId));
+    });
+  }
+
+  /**
+   * Process active subscription if exists or creates new one,
+   *
+   * @param subscription
+   */
+  processSubscription(subscription: any): void {
+    let ramValue = this.value * 1024; //TODO
+
+    let promise;
+    // check subscription exists:
+    if (subscription) {
+      let packages = angular.copy(subscription.packages);
+
+      // try to update RAM package:
+      let ramPackage = this.lodash.find(packages, (pckg: any) => {
+        return pckg.templateId === this.ramPackage.id;
+      });
+
+      if (ramPackage) {
+        let ramResource = this.lodash.find(ramPackage.resources, (resource: any) => {
+          return resource.type === CodenvyResourceLimits.RAM;
+        });
+        // check RAM resource was defined:
+        if (ramResource) {
+          ramResource.amount += ramValue;
+        } else { // process no RAM resource:
+          ramPackage.resources.push(this.prepareRAMResource(ramValue));
+        }
+      } else { // process no RAM package:
+        let resources = [this.prepareRAMResource(ramValue)];
+        packages.push({resources: resources});
+      }
+      promise = this.codenvySubscription.updateSubscription(this.accountId, packages);
+    } else { // process no active subscription:
+      let packages = [];
+      let resources = [this.prepareRAMResource(ramValue)];
+      packages.push({resources: resources});
+      promise = this.codenvySubscription.createSubscription(this.accountId, packages);
+    }
+
+    promise.then(() => {
+      this.isLoading = false;
+      this.callbackController.onRAMChanged();
+      this.hide();
+    }, (error: any) => {
+      this.isLoading = false;
+      this.cheNotification.showError(error.data && error.data.message ? error.data.message : 'Failed to add more RAM to account.');
+    });
+  }
+
+  /**
+   * Returns RAM resource based on provided RAM amount.
+   *
+   * @param value RAM amount
+   * @returns any ram resource
+   */
+  prepareRAMResource(value: number): any {
+    return {amount: value, unit: 'mb', type: CodenvyResourceLimits.RAM};
+  }
+}
---
+++ dashboard/src/components/api/codenvy-subscription.factory.ts
@@ -0,0 +1,154 @@
+/*
+ *  [2015] - [2016] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+'use strict';
+import IResource = angular.resource.IResource;
+
+/**
+ * This class is handling the credit card API.
+ * @author Ann Shumilova
+ */
+export class CodenvySubscription {
+  $resource: ng.resource.IResourceService
+  $q: ng.IQService;
+  $log: ng.ILogService;
+
+  subscriptionsPerAccount: Map<string, any>;
+  licensesPerAccount: Map<string, any>;
+  packages: Array<any>;
+
+  remoteSubscriptionAPI: any;
+
+  /**
+   * Default constructor that is using resource
+   * @ngInject for Dependency injection
+   */
+  constructor ($resource: ng.resource.IResourceService, $q: ng.IQService, $log: ng.ILogService) {
+    // keep resource
+    this.$resource = $resource;
+    this.$q = $q;
+    this.$log = $log;
+
+    this.subscriptionsPerAccount = new Map();
+    this.licensesPerAccount = new Map();
+    this.packages = [];
+
+    // remote call
+    this.remoteSubscriptionAPI = this.$resource('/api/subscription/:accountId', {}, {
+      getActiveSubscription: {method: 'GET', url: '/api/subscription/:accountId'},
+      createSubscription: {method: 'POST', url: '/api/subscription/:accountId'},
+      updateSubscription: {method: 'PUT', url: '/api/subscription/:accountId'},
+      getLicense: {method: 'GET', url: '/api/license/account/:accountId'},
+      getPackages: {method: 'GET', url: '/api/package', isArray: true}
+    });
+
+  }
+
+  /**
+   * Fetch active subscription by account id.
+   *
+   * @param accountId {string}
+   * @return {ng.IPromise<any>}
+   */
+  fetchActiveSubscription(accountId: string): ng.IPromise<any> {
+    let promise = this.remoteSubscriptionAPI.getActiveSubscription({accountId: accountId}).$promise;
+
+    let resultPromise = promise.then((data: any) => {
+      this.subscriptionsPerAccount.set(accountId, data);
+    });
+    return resultPromise;
+  }
+
+  /**
+   * Returns active subscription for pointed account if exists.
+   *
+   * @param accountId account's id
+   * @returns {any}
+   */
+  getActiveSubscription(accountId: string): any {
+    return this.subscriptionsPerAccount.get(accountId);
+  }
+
+  /**
+   * Updates active subscription.
+   *
+   * @param accountId {string} account's id
+   * @param subscription subscription to be updated
+   * @return {IPromise<{method, url}|any|void|PromiseLike<void>|IDBRequest>}
+   */
+  updateSubscription(accountId: string, subscription: any): ng.IPromise<any> {
+    return this.remoteSubscriptionAPI.updateSubscription({accountId: accountId}, subscription).$promise;
+  }
+
+  /**
+   * Creates new subscription.
+   *
+   * @param accountId {string} account's id
+   * @param subscription subscription to be created
+   * @return {IPromise<{method, url}|any|void|PromiseLike<void>|IDBRequest>}
+   */
+  createSubscription(accountId: string, subscription: any): ng.IPromise<any> {
+    return this.remoteSubscriptionAPI.createSubscription({accountId: accountId}, subscription).$promise;
+  }
+
+  /**
+   * Fetch license by account id.
+   *
+   * @param accountId account's id
+   * @returns {PromiseLike<TResult>|Promise<TResult>}
+   */
+  fetchLicense(accountId: string): ng.IPromise<any> {
+    let promise = this.remoteSubscriptionAPI.getLicense({accountId: accountId}).$promise;
+
+    let resultPromise = promise.then((data: any) => {
+      this.licensesPerAccount.set(accountId, data);
+    });
+
+    return resultPromise;
+  }
+
+  /**
+   * Returns license for pointed account.
+   *
+   * @param accountId account's id
+   * @returns {any}
+   */
+  getLicense(accountId: string): any {
+    return this.licensesPerAccount.get(accountId);
+  }
+
+  /**
+   * Fetches defined subscription packages.
+   *
+   * @returns {PromiseLike<TResult>|Promise<TResult>}
+   */
+  fetchPackages(): ng.IPromise<any> {
+    let promise = this.remoteSubscriptionAPI.getPackages().$promise;
+
+    let resultPromise = promise.then((data: any) => {
+      this.packages = data;
+    });
+
+    return resultPromise;
+  }
+
+  /**
+   * Returns the list of defined packages.
+   *
+   * @returns {Array<any>}
+   */
+  getPackages(): Array<any> {
+    return this.packages;
+  }
+}
