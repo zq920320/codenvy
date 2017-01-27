8857702effc629b7e7dc36a0f1aa6a932a0d540f
Mykola Morhun <mmorhun@codenvy.com>
2017-01-27 11:53:23 +0200

    CODENVY-1498: Cleanup snapshots images after workspace start (#1566)
    CODENVY-1498: Cleanup snapshots images after workspace images build

--- plugins/plugin-hosted/codenvy-machine-hosted/src/main/java/com/codenvy/machine/HostedMachineProviderImpl.java
+++ plugins/plugin-hosted/codenvy-machine-hosted/src/main/java/com/codenvy/machine/HostedMachineProviderImpl.java
@@ -16,6 +16,7 @@ package com.codenvy.machine;
 
 import com.codenvy.machine.authentication.server.MachineTokenRegistry;
 import com.google.common.base.MoreObjects;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import org.eclipse.che.api.core.NotFoundException;
 import org.eclipse.che.api.core.model.machine.ServerConf;
@@ -25,6 +26,7 @@ import org.eclipse.che.api.machine.server.exception.MachineException;
 import org.eclipse.che.api.machine.server.exception.SourceNotFoundException;
 import org.eclipse.che.commons.annotation.Nullable;
 import org.eclipse.che.commons.env.EnvironmentContext;
+import org.eclipse.che.commons.lang.concurrent.LoggingUncaughtExceptionHandler;
 import org.eclipse.che.commons.lang.os.WindowsPathEscaper;
 import org.eclipse.che.plugin.docker.client.DockerConnector;
 import org.eclipse.che.plugin.docker.client.ProgressMonitor;
@@ -34,17 +36,24 @@ import org.eclipse.che.plugin.docker.client.params.BuildImageParams;
 import org.eclipse.che.plugin.docker.machine.DockerInstanceStopDetector;
 import org.eclipse.che.plugin.docker.machine.DockerMachineFactory;
 import org.eclipse.che.plugin.docker.machine.MachineProviderImpl;
+import org.slf4j.Logger;
 
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static com.codenvy.machine.MaintenanceConstraintProvider.MAINTENANCE_CONSTRAINT_KEY;
 import static com.codenvy.machine.MaintenanceConstraintProvider.MAINTENANCE_CONSTRAINT_VALUE;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Specific implementation of {@link MachineProviderImpl} needed for hosted environment.
@@ -57,14 +66,19 @@ import static com.codenvy.machine.MaintenanceConstraintProvider.MAINTENANCE_CONS
  * @author Anton Korneta
  * @author Roman Iuvshyn
  * @author Alexander Garagatyi
+ * @author Mykola Morhun
  */
 public class HostedMachineProviderImpl extends MachineProviderImpl {
+    private static final Logger LOG = getLogger(HostedMachineProviderImpl.class);
+
     private final DockerConnector                               docker;
     private final UserSpecificDockerRegistryCredentialsProvider dockerCredentials;
     private final MachineTokenRegistry                          tokenRegistry;
     private final String                                        cpusetCpus;
     private final long                                          cpuPeriod;
     private final long                                          cpuQuota;
+
+    private final ScheduledExecutorService snapshotImagesCleanerService;
 
     @Inject
     public HostedMachineProviderImpl(DockerConnector docker,
@@ -122,6 +136,12 @@ public class HostedMachineProviderImpl extends MachineProviderImpl {
         this.cpusetCpus = cpusetCpus;
         this.cpuPeriod = cpuPeriod;
         this.cpuQuota = cpuQuota;
+
+        this.snapshotImagesCleanerService = Executors.newSingleThreadScheduledExecutor(
+                new ThreadFactoryBuilder().setNameFormat("SnapshotImagesCleaner")
+                                          .setUncaughtExceptionHandler(LoggingUncaughtExceptionHandler.getInstance())
+                                          .setDaemon(false)
+                                          .build());
     }
 
     @Override
@@ -176,7 +196,33 @@ public class HostedMachineProviderImpl extends MachineProviderImpl {
             if (workDir != null) {
                 FileCleaner.addFile(workDir);
             }
+            // When new image is being built it pulls base image. This operation is performed by docker build command.
+            // So, after build it is needed to cleanup base image if it is a snapshot.
+            if (service.getImage().contains(MACHINE_SNAPSHOT_PREFIX)) {
+                submitCleanSnapshotImageTask(service.getImage());
+            }
         }
+    }
+
+    /**
+     * Sometimes swarm cannot delete image after its pull during a few seconds.
+     * To workaround that problem and avoid redundant delay on workspace start
+     * we must clean up snapshot image in separate thread after some delay.
+     *
+     * @param image
+     *         image to clean, e.g. 172.11.12.13:5000/machine_snapshot_abcdef1234567890:latest
+     */
+    private void submitCleanSnapshotImageTask(String image) {
+        // TODO replace this method by docker.removeImage(image) call after fix of the problem in pure Docker Swarm
+        snapshotImagesCleanerService.schedule(() -> {
+            try {
+                docker.removeImage(image);
+            } catch (IOException e) {
+                if (!e.getMessage().contains("No such image")) { // ignore error if image already deleted
+                    LOG.error("Failed to delete pulled snapshot: " + image);
+                }
+            }
+        }, 10L, TimeUnit.SECONDS);
     }
 
     /**
@@ -227,4 +273,21 @@ public class HostedMachineProviderImpl extends MachineProviderImpl {
             }
         }
     }
+
+    @PreDestroy
+    private void finalizeSnapshotImagesCleaner() {
+        snapshotImagesCleanerService.shutdown();
+        try {
+            if (!snapshotImagesCleanerService.awaitTermination(30L, TimeUnit.SECONDS)) {
+                snapshotImagesCleanerService.shutdownNow();
+                if (!snapshotImagesCleanerService.awaitTermination(10L, TimeUnit.SECONDS)) {
+                    LOG.warn("Failed to terminate SnapshotImagesCleaner scheduler");
+                }
+            }
+        } catch (InterruptedException e) {
+            snapshotImagesCleanerService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
 }
