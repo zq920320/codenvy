21f4459b8c7265a5cd998b9321770b991a852530
Tyler Jewell <tjewell@codenvy.com>
2017-01-29 15:29:44 -0800

    fixup

--- dockerfiles/cli/scripts/cli.sh
+++ dockerfiles/cli/scripts/cli.sh
@@ -6,7 +6,7 @@
 # http://www.eclipse.org/legal/epl-v10.html
 #
 
-cli_pre_init() {
+cli_post_init() {
   GLOBAL_HOST_IP=${GLOBAL_HOST_IP:=$(docker_run --net host ${UTILITY_IMAGE_CHEIP})}
   DEFAULT_CODENVY_HOST=$GLOBAL_HOST_IP
   CODENVY_HOST=${CODENVY_HOST:-${DEFAULT_CODENVY_HOST}}
